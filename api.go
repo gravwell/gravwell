4aea0502a8d931cd1610368dd3b38648c6415b9b
Kris Watts <kris.watts@gravwell.io>
2018-05-25 10:41:53 -0600

    Adding version stuff

---
+++ api.go
@@ -0,0 +1,24 @@
+/*************************************************************************
+ * Copyright 2017 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package ingest
+
+import (
+	"fmt"
+	"io"
+)
+
+const (
+	//MAJOR API VERSIONS should always be compatible, there just may be additional features
+	API_VERSION_MAJOR uint32 = 0
+	API_VERSION_MINOR uint32 = 1
+)
+
+func PrintVersion(wtr io.Writer) {
+	fmt.Fprintf(wtr, "API Version:\t%d.%d\n", API_VERSION_MAJOR, API_VERSION_MINOR)
+}
