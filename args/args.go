3ac289d8793985697ace3ec07a645cc4c46796fc
Kris Watts <kris.watts@gravwell.io>
2018-08-27 11:18:53 -0600

    adding single file ingester and an args library so that we don't have to keep writing the same code over and over

---
+++ args/args.go
@@ -0,0 +1,108 @@
+/*************************************************************************
+ * Copyright 2017 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package args
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gravwell/ingest"
+	"github.com/gravwell/ingest/config"
+)
+
+var (
+	tagName         = flag.String("tag-name", "default", "Tag name for ingested data")
+	pipeConns       = flag.String("pipe-conn", "", "Path to pipe connection")
+	clearConns      = flag.String("clear-conns", "", "comma seperated server:port list of cleartext targets")
+	tlsConns        = flag.String("tls-conns", "", "comma seperated server:port list of TLS connections")
+	tlsPublicKey    = flag.String("tls-public-key", "", "Path to TLS public key")
+	tlsPrivateKey   = flag.String("tls-private-key", "", "Path to TLS private key")
+	tlsRemoteVerify = flag.Bool("tls-remote-verify", true, "Validate remote TLS certificates")
+	ingestSecret    = flag.String("ingest-secret", "IngestSecrets", "Ingest key")
+	timeoutSec      = flag.Int("timeout", 1, "Connection timeout in seconds")
+)
+
+type Args struct {
+	Tags            []string
+	Conns           []string
+	TLSPublicKey    string
+	TLSPrivateKey   string
+	TLSRemoteVerify bool
+	IngestSecret    string
+	Timeout         time.Duration
+}
+
+func Parse() (a Args, err error) {
+	flag.Parse()
+	if *timeoutSec < 0 {
+		err = errors.New("Invalid timeout")
+		return
+	}
+	a.Timeout = time.Second * time.Duration(*timeoutSec)
+	if *tagName == "" {
+		err = errors.New("tag name required")
+		return
+	} else {
+		//verify that the tag name is valid
+		*tagName = strings.TrimSpace(*tagName)
+		if strings.ContainsAny(*tagName, ingest.FORBIDDEN_TAG_SET) {
+			err = errors.New("Forbidden characters in tag")
+			return
+		}
+	}
+	a.Tags = []string{*tagName}
+	if *clearConns != "" {
+		for _, conn := range strings.Split(*clearConns, ",") {
+			conn = strings.TrimSpace(conn)
+			if len(conn) > 0 {
+				conn = fmt.Sprintf("tcp://%s", config.AppendDefaultPort(conn, config.DefaultCleartextPort))
+				a.Conns = append(a.Conns, conn)
+			}
+		}
+	}
+	if *tlsConns != "" {
+		for _, conn := range strings.Split(*tlsConns, ",") {
+			conn = strings.TrimSpace(conn)
+			if len(conn) > 0 {
+				conn = fmt.Sprintf("tls://%s", config.AppendDefaultPort(conn, config.DefaultTLSPort))
+				a.Conns = append(a.Conns, fmt.Sprintf("tls://%s", conn))
+			}
+		}
+	}
+	if *pipeConns != "" {
+		for _, conn := range strings.Split(*pipeConns, ",") {
+			conn = strings.TrimSpace(conn)
+			if len(conn) > 0 {
+				a.Conns = append(a.Conns, fmt.Sprintf("pipe://%s", conn))
+			}
+		}
+	}
+	if len(a.Conns) == 0 {
+		err = errors.New("No indexer connections specified")
+		return
+	}
+	a.TLSPublicKey = *tlsPublicKey
+	a.TLSPrivateKey = *tlsPrivateKey
+	if a.TLSPublicKey != "" && a.TLSPrivateKey == "" {
+		err = errors.New("A private key is required when specifying a public key")
+		return
+	} else if a.TLSPublicKey == "" && a.TLSPrivateKey != "" {
+		err = errors.New("A public key is required when specifying a private key")
+		return
+	}
+	a.TLSRemoteVerify = *tlsRemoteVerify
+	a.IngestSecret = *ingestSecret
+	if a.IngestSecret == "" {
+		err = errors.New("Ingest secret required")
+	}
+	return
+}
---
+++ singleFile/main.go
@@ -0,0 +1,148 @@
+/*************************************************************************
+ * Copyright 2017 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gravwell/ingest"
+	"github.com/gravwell/ingest/entry"
+	"github.com/gravwell/ingesters/args"
+	"github.com/gravwell/ingesters/version"
+	"github.com/gravwell/timegrinder"
+)
+
+var (
+	tso    = flag.String("timestamp-override", "", "Timestamp override")
+	inFile = flag.String("i", "", "Input file to process")
+	ver    = flag.Bool("v", false, "Print version and exit")
+	utc    = flag.Bool("utc", false, "Assume UTC time")
+
+	nlBytes = []byte("\n")
+)
+
+func init() {
+	flag.Parse()
+	if *ver {
+		version.PrintVersion(os.Stdout)
+		ingest.PrintVersion(os.Stdout)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	if *inFile == "" {
+		log.Fatal("Input file path required")
+	}
+	timestampOverride := -1
+	a, err := args.Parse()
+	if err != nil {
+		log.Fatalf("Invalid arguments: %v\n", err)
+	}
+	if len(a.Tags) != 1 {
+		log.Fatal("File oneshot only accepts a single tag")
+	}
+
+	//resolve the timestmap override if there is one
+	if *tso != "" {
+		if timestampOverride, err = timegrinder.FormatDirective(*tso); err != nil {
+			log.Fatalf("Invalid timestamp override: %v\n", err)
+		}
+	}
+
+	//get a handle on the input file
+	fin, err := os.Open(*inFile)
+	if err != nil {
+		log.Fatalf("Failed to open %s: %v\n", *inFile, err)
+	}
+
+	//fire up a uniform muxer
+	igst, err := ingest.NewUniformIngestMuxer(a.Conns, a.Tags, a.IngestSecret, a.TLSPublicKey, a.TLSPrivateKey, "")
+	if err != nil {
+		log.Fatalf("Failed to create new ingest muxer: %v\n", err)
+	}
+	if err := igst.Start(); err != nil {
+		log.Fatalf("Failed to start ingest muxer: %v\n", err)
+	}
+	if err := igst.WaitForHot(a.Timeout); err != nil {
+		log.Fatalf("Failed to wait for hot connection: %v\n", err)
+	}
+	tag, err := igst.GetTag(a.Tags[0])
+	if err != nil {
+		log.Fatalf("Failed to resolve tag %s: %v\n", a.Tags[0], err)
+	}
+
+	//go ingest the file
+	if err := ingestFile(fin, igst, tag, timestampOverride); err != nil {
+		log.Fatalf("Failed to ingest file: %v\n", err)
+	}
+
+	if err = igst.Sync(a.Timeout); err != nil {
+		log.Fatalf("Failed to sync ingest muxer: %v\n", err)
+	}
+	if err := igst.Close(); err != nil {
+		log.Fatalf("Failed to close the ingest muxer: %v\n", err)
+	}
+	if err := fin.Close(); err != nil {
+		log.Fatalf("Failed to close the input file: %v\n", err)
+	}
+}
+
+func ingestFile(fin *os.File, igst *ingest.IngestMuxer, tag entry.EntryTag, tso int) error {
+	var bts []byte
+	var ts time.Time
+	var ok bool
+	//build a new timegrinder
+	c := timegrinder.Config{
+		EnableLeftMostSeed: true,
+	}
+	if tso > 0 {
+		c.FormatOverride = tso
+	}
+	tg, err := timegrinder.NewTimeGrinder(c)
+	if err != nil {
+		return err
+	}
+	if *utc {
+		tg.SetUTC()
+	}
+
+	src, err := igst.SourceIP()
+	if err != nil {
+		return err
+	}
+
+	scn := bufio.NewScanner(fin)
+	for scn.Scan() {
+		if bts = bytes.TrimSuffix(scn.Bytes(), nlBytes); len(bts) == 0 {
+			continue
+		}
+		if ts, ok, err = tg.Extract(bts); err != nil {
+			return err
+		} else if !ok {
+			ts = time.Now()
+		}
+		ent := &entry.Entry{
+			TS:  entry.FromStandard(ts),
+			Tag: tag,
+			SRC: src,
+		}
+		ent.Data = append(ent.Data, bts...) //force reallocation due to the scanner
+		if err = igst.WriteEntry(ent); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
