0dbf67efde1cb32500af572c3fc2af36e2b092f0
Kris Watts <kris.watts@gravwell.io>
2018-09-19 10:35:18 -0600

    updating build date

--- version/version.go
+++ version/version.go
@@ -17,11 +17,11 @@ import (
 const (
 	MajorVersion int = 2
 	MinorVersion int = 2
-	PointVersion int = 5
+	PointVersion int = 7
 )
 
 var (
-	BuildDate time.Time = time.Date(2018, 6, 13, 12, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2018, 9, 19, 12, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
