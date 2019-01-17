73c98b4e44d2805598d23113d057a2109aec2c31
Kris Watts <kris.watts@gravwell.io>
2019-01-17 15:28:22 -0700

    adding a generic HTTP post ingester

---
+++ HttpPostIngester/config.go
@@ -0,0 +1,169 @@
+/*************************************************************************
+ * Copyright 2018 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/gravwell/ingest"
+	"github.com/gravwell/ingest/config"
+
+	"gopkg.in/gcfg.v1"
+)
+
+const (
+	maxConfigSize  int64 = (1024 * 1024 * 2) //2MB, even this is crazy large
+	defaultMaxBody int   = 4 * 1024 * 1024
+	defaultLogLoc        = `/opt/gravwell/log/gravwell_http_ingester.log`
+)
+
+type gbl struct {
+	config.IngestConfig
+	Bind         string
+	Max_Body     int
+	Log_Location string
+}
+
+type cfgReadType struct {
+	Global   gbl
+	Listener map[string]*lst
+}
+
+type lst struct {
+	URL      string //the URL we will listen to
+	Tag_Name string //the tag to assign to the request
+}
+
+type cfgType struct {
+	gbl
+	Listener map[string]*lst
+}
+
+func GetConfig(path string) (*cfgType, error) {
+	var content []byte
+	fin, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	fi, err := fin.Stat()
+	if err != nil {
+		fin.Close()
+		return nil, err
+	}
+	//This is just a sanity check
+	if fi.Size() > maxConfigSize {
+		fin.Close()
+		return nil, errors.New("Config File Far too large")
+	}
+	content = make([]byte, fi.Size())
+	n, err := fin.Read(content)
+	fin.Close()
+	if int64(n) != fi.Size() {
+		return nil, errors.New("Failed to read config file")
+	}
+
+	var cr cfgReadType
+	if err := gcfg.ReadStringInto(&cr, string(content)); err != nil {
+		return nil, err
+	}
+	c := &cfgType{
+		gbl:      cr.Global,
+		Listener: cr.Listener,
+	}
+	if err := verifyConfig(c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+func verifyConfig(c *cfgType) error {
+	if err := c.IngestConfig.Verify(); err != nil {
+		return err
+	}
+	if c.Bind == `` {
+		return fmt.Errorf("No bind string specified")
+	}
+	urls := map[string]string{}
+	if len(c.Listener) == 0 {
+		return errors.New("No Sniffers specified")
+	}
+	for k, v := range c.Listener {
+		var pth string
+		if len(v.URL) == 0 {
+			return errors.New("No URL provided for " + k)
+		}
+		p, err := url.Parse(v.URL)
+		if err != nil {
+			return fmt.Errorf("URL structure is invalid: %v", err)
+		}
+		if p.Scheme != `` {
+			return errors.New("May not specify scheme in listening URL")
+		} else if p.Host != `` {
+			return errors.New("May not specify host in listening URL")
+		}
+		pth = p.Path
+
+		if orig, ok := urls[pth]; ok {
+			return fmt.Errorf("URL %s duplicated in %s (was in %s)", v.URL, k, orig)
+		}
+		urls[pth] = k
+		if len(v.Tag_Name) == 0 {
+			v.Tag_Name = `default`
+		}
+		if strings.ContainsAny(v.Tag_Name, ingest.FORBIDDEN_TAG_SET) {
+			return errors.New("Invalid characters in the \"" + v.Tag_Name + "\"Tag-Name for " + k)
+		}
+		//normalize the path
+		v.URL = pth
+		c.Listener[k] = v
+	}
+	if len(urls) == 0 {
+		return fmt.Errorf("No listeners specified")
+	}
+	return nil
+}
+
+// Generate a list of all tags used by this ingester
+func (c *cfgType) Tags() (tags []string, err error) {
+	tagMp := make(map[string]bool, 1)
+	for _, v := range c.Listener {
+		if len(v.Tag_Name) == 0 {
+			continue
+		}
+		if _, ok := tagMp[v.Tag_Name]; !ok {
+			tags = append(tags, v.Tag_Name)
+			tagMp[v.Tag_Name] = true
+		}
+	}
+	if len(tags) == 0 {
+		err = errors.New("No tags specified")
+	} else {
+		sort.Strings(tags)
+	}
+	return
+}
+
+func (c *cfgType) LogLoc() string {
+	if c.Log_Location == `` {
+		return defaultLogLoc
+	}
+	return c.Log_Location
+}
+
+func (c *cfgType) MaxBody() int {
+	if c.Max_Body <= 0 {
+		return defaultMaxBody
+	}
+	return c.Max_Body
+}
---
+++ HttpPostIngester/main.go
@@ -0,0 +1,224 @@
+/*************************************************************************
+ * Copyright 2018 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	dlog "log"
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gravwell/ingest"
+	"github.com/gravwell/ingest/entry"
+	"github.com/gravwell/ingest/log"
+	"github.com/gravwell/ingesters/version"
+)
+
+const (
+	defaultConfigLoc = `/opt/gravwell/etc/gravwell_http_ingester.conf`
+)
+
+var (
+	confLoc = flag.String("config-file", defaultConfigLoc, "Location for configuration file")
+	verbose = flag.Bool("v", false, "Display verbose status updates to stdout")
+	ver     = flag.Bool("version", false, "Print the version information and exit")
+	lg      *log.Logger
+	v       bool
+	maxBody int
+)
+
+func init() {
+	flag.Parse()
+	if *ver {
+		version.PrintVersion(os.Stdout)
+		ingest.PrintVersion(os.Stdout)
+		os.Exit(0)
+	} else if *verbose {
+		v = true
+	}
+	if *confLoc == `` {
+		dlog.Fatal("Invalid log location")
+	}
+	lg = log.New(os.Stderr) // DO NOT close this, it will prevent backtraces from firing
+}
+
+type handler struct {
+	mp   map[string]entry.EntryTag
+	igst *ingest.IngestMuxer
+}
+
+func main() {
+	cfg, err := GetConfig(*confLoc)
+	if err != nil {
+		lg.Fatal("Failed to load config file \"%s\": %v", *confLoc, err)
+	}
+	if cfg.LogLoc() != `` {
+		fout, err := os.OpenFile(cfg.LogLoc(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0640)
+		if err != nil {
+			dlog.Fatal(err)
+		}
+		if err = lg.AddWriter(fout); err != nil {
+			dlog.Fatalf("Failed to add a writer: %v", err)
+		}
+		defer fout.Close()
+	}
+	maxBody = cfg.MaxBody()
+
+	debugout("Handling %d listeners", len(cfg.Listener))
+	tags, err := cfg.Tags()
+	if err != nil {
+		lg.Fatal("Failed to load tags: %v", err)
+	}
+	conns, err := cfg.Targets()
+	if err != nil {
+		lg.Fatal("Failed to get backend targets from configuration: %v", err)
+	}
+
+	debugout("Loaded %d tags", len(tags))
+	igCfg := ingest.UniformMuxerConfig{
+		Destinations: conns,
+		Tags:         tags,
+		Auth:         cfg.Secret(),
+		LogLevel:     cfg.LogLevel(),
+		IngesterName: "httppost",
+		VerifyCert:   !cfg.InsecureSkipTLSVerification(),
+		Logger:       lg,
+	}
+	if cfg.EnableCache() {
+		igCfg.EnableCache = true
+		igCfg.CacheConfig.FileBackingLocation = cfg.LocalFileCachePath()
+		igCfg.CacheConfig.MaxCacheSize = cfg.MaxCachedData()
+	}
+	igst, err := ingest.NewUniformMuxer(igCfg)
+	if err != nil {
+		lg.Fatal("Failed to create new uniform muxer: %v ", err)
+	}
+	debugout("Started ingester muxer\n")
+	if err := igst.Start(); err != nil {
+		lg.Fatal("Failed start our ingest system: %v", err)
+	}
+	debugout("Waiting for connections to indexers\n")
+	if err := igst.WaitForHot(cfg.Timeout()); err != nil {
+		lg.Fatal("Timedout waiting for backend connections: %v", err)
+	}
+	debugout("Successfully connected to ingesters\n")
+	hnd := &handler{
+		mp:   map[string]entry.EntryTag{},
+		igst: igst,
+	}
+	for _, v := range cfg.Listener {
+		tg, err := igst.GetTag(v.Tag_Name)
+		if err != nil {
+			lg.Fatal("Failed to pull tag %v: %v", v.Tag_Name, err)
+		}
+		hnd.mp[v.URL] = tg
+	}
+	srv := &http.Server{
+		Addr:         cfg.Bind,
+		Handler:      hnd,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	defer igst.Sync(time.Second)
+	defer igst.Close()
+	if err := srv.ListenAndServe(); err != nil {
+		lg.Error("Failed to serve HTTP server: %v", err)
+	}
+
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	if r.Method != http.MethodPost {
+		lg.Info("bad request Method: %s != %s", r.Method, http.MethodPost)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	tg, ok := h.mp[r.URL.Path]
+	if !ok {
+		lg.Info("bad request URL %v", r.URL.Path)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	b := make([]byte, maxBody)
+	n, err := readAll(r.Body, b)
+	if err != nil && err != io.EOF {
+		lg.Error("Got bad request: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else if n == maxBody {
+		lg.Error("Request too large, 4MB max")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	b = b[0:n]
+	if len(b) == 0 {
+		lg.Warn("Got an empty post from", r.RemoteAddr)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	e := entry.Entry{
+		TS:   entry.Now(),
+		SRC:  getRemoteIP(r),
+		Tag:  tg,
+		Data: b,
+	}
+	if err = h.igst.WriteEntry(&e); err != nil {
+		lg.Error("Failed to send entry: %v", err)
+	}
+}
+
+func debugout(format string, args ...interface{}) {
+	if !v {
+		return
+	}
+	fmt.Printf(format, args...)
+}
+
+func getRemoteAddr(r *http.Request) (host string) {
+	xfflist, ok := r.Header[`X-Forwarded-For`]
+	if !ok || len(xfflist) == 0 {
+		host, _, _ = net.SplitHostPort(r.RemoteAddr)
+	} else {
+		host = xfflist[0]
+	}
+	return
+
+}
+
+func getRemoteIP(r *http.Request) (ip net.IP) {
+	if host := getRemoteAddr(r); host != `` {
+		if ip = net.ParseIP(host); ip != nil {
+			return
+		}
+	}
+	ip = net.ParseIP(`127.0.0.1`)
+	return
+}
+
+func readAll(r io.Reader, buff []byte) (offset int, err error) {
+	var n int
+	for offset < len(buff) {
+		if n, err = r.Read(buff[offset:]); err != nil {
+			if err == io.EOF {
+				err = nil
+				offset += n
+			}
+			return
+		} else if n == 0 {
+			return
+		}
+		offset += n
+	}
+	return
+}
