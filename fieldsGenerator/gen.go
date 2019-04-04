737040d6b54fb22247b7607ca5ea48cd845642a5
kristopher watts <kris.watts@gravwell.io>
2019-04-04 17:27:16 -0600

    adding fields generator

---
+++ fieldsGenerator/gen.go
@@ -0,0 +1,87 @@
+/*************************************************************************
+ * Copyright 2018 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	rd "github.com/Pallinder/go-randomdata"
+	"github.com/gravwell/ingest"
+	"github.com/gravwell/ingest/entry"
+)
+
+const (
+	streamBlock = 10
+)
+
+func throw(igst *ingest.IngestMuxer, tag entry.EntryTag, cnt uint64, dur time.Duration) (err error) {
+	sp := dur / time.Duration(cnt)
+	ts := time.Now()
+	for i := uint64(0); i < cnt; i++ {
+		dt := genData(ts)
+		if err = igst.WriteEntry(&entry.Entry{
+			TS:   entry.FromStandard(ts),
+			Tag:  tag,
+			SRC:  src,
+			Data: dt,
+		}); err != nil {
+			return
+		}
+		ts = ts.Add(-1 * sp)
+		totalBytes += uint64(len(dt))
+	}
+	return
+}
+
+func stream(igst *ingest.IngestMuxer, tag entry.EntryTag, cnt uint64) (err error) {
+	var blksize uint64
+	if cnt < streamBlock {
+		blksize = 1
+	} else {
+		blksize = streamBlock
+	}
+	sp := time.Second / time.Duration((cnt / blksize))
+
+	for {
+		for i := uint64(0); i < blksize; i++ {
+			ts := time.Now()
+			dt := genData(ts)
+			if err = igst.WriteEntry(&entry.Entry{
+				TS:   entry.FromStandard(ts),
+				Tag:  tag,
+				SRC:  src,
+				Data: dt,
+			}); err != nil {
+				return
+			}
+			totalBytes += uint64(len(dt))
+		}
+		time.Sleep(sp)
+	}
+	return
+}
+
+func genData(ts time.Time) []byte {
+	ipa, ipb := ips()
+	return []byte(fmt.Sprintf("%s%s%s%s%s%s%d%s%s%s%d%s\"%s\"",
+		ts.Format(tsFormat), delim, getApp(), delim,
+		ipa, delim, 1+rand.Intn(2048), delim,
+		ipb, delim, 2048+rand.Intn(0xffff-2048), delim,
+		rd.Paragraph()))
+}
+
+func ips() (string, string) {
+	if (rand.Int() & 3) == 0 {
+		//more IPv4 than 6
+		return rd.IpV6Address(), rd.IpV6Address()
+	}
+	return rd.IpV4Address(), rd.IpV4Address()
+}
