2776a607448b58c43d8d1e52539f932e0062d504
Kris Watts <kris.watts@gravwell.io>
2019-04-18 12:11:03 -0600

    adding canbus ingester

---
+++ canbus/canbus.go
@@ -0,0 +1,143 @@
+/*************************************************************************
+ * Copyright 2017 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package main
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"net"
+
+	"golang.org/x/sys/unix"
+)
+
+const (
+	minBuff      int = 16
+	rechargeSize int = 1024 * 1024
+)
+
+var (
+	ErrFailedPacketRead = errors.New("Failed to read complete CAN packet")
+)
+
+type Cansock struct {
+	fd   int
+	sock *unix.SockaddrCAN
+	buff []byte
+}
+
+// Create a new CAN device bound to device such as can0 or vcan0
+func New(dev string) (*Cansock, error) {
+	//create the socket
+	fd, err := unix.Socket(unix.AF_CAN, unix.SOCK_RAW, unix.CAN_RAW)
+	if err != nil {
+		return nil, err
+	}
+	//get the interface for indexing
+	iface, err := net.InterfaceByName(dev)
+	if err != nil {
+		unix.Close(fd)
+		return nil, err
+	}
+	addr := &unix.SockaddrCAN{Ifindex: iface.Index}
+	//actually bind the socket to the interface
+	if err := unix.Bind(fd, addr); err != nil {
+		unix.Close(fd)
+		return nil, err
+	}
+
+	return &Cansock{
+		fd:   fd,
+		sock: &unix.SockaddrCAN{Ifindex: iface.Index},
+	}, nil
+}
+
+func (c *Cansock) Close() error {
+	if c.fd == 0 || c.sock == nil {
+		return nil
+	}
+	if err := unix.Close(c.fd); err != nil {
+		return err
+	}
+	c.fd = 0
+	c.sock = nil
+	return nil
+}
+
+func (c *Cansock) Read() ([]byte, error) {
+	if len(c.buff) < minBuff {
+		c.buff = make([]byte, rechargeSize)
+	}
+	n, err := unix.Read(c.fd, c.buff[:minBuff])
+	if err != nil {
+		return nil, err
+	}
+	r := c.buff[:n]
+	c.buff = c.buff[n:]
+
+	pkt, err := packPacket(r)
+	if err != nil {
+		return nil, err
+	}
+	return pkt, nil
+}
+
+func packPacket(r []byte) (pkt []byte, err error) {
+	//ensure incoming packet has the correct size
+	if len(r) != minBuff {
+		err = ErrFailedPacketRead
+		return
+	}
+	pkt = r
+	//rectify the packet based on data length (we basically just chop off the CRC)
+	canlen := r[4] & 0x0f
+	for i := 0; i < int(canlen); i++ {
+		pkt[5+i] = pkt[8+i]
+	}
+	pkt = pkt[:5+canlen]
+	return
+}
+
+type CanPacket struct {
+	ID       uint32
+	Extended bool
+	RTR      bool
+	Data     []byte
+}
+
+func ExtractPacket(d []byte) (pkt CanPacket, err error) {
+	//we need enough to get the address, length and bitfield out
+	if len(d) < 5 {
+		err = ErrInvalidPacket
+		return
+	}
+	//pull the length field
+	l := d[4] & 0xf
+	if len(d) < int(5+l) {
+		err = ErrInvalidPacket
+		return
+	}
+	pkt.Data = d[5:]
+	pkt.Extended = (d[3]&0x80 == 0x80)
+	pkt.RTR = (d[3]&0x40 == 0x40)
+	if !pkt.Extended {
+		pkt.ID = binary.LittleEndian.Uint32(d[:4]) & unix.CAN_SFF_MASK
+	} else {
+		pkt.ID = binary.LittleEndian.Uint32(d[:4]) & unix.CAN_EFF_MASK
+	}
+	return
+}
+
+func (cp CanPacket) String() string {
+	str := fmt.Sprintf("%.08x", cp.ID)
+	if cp.RTR {
+		str += " RTR"
+	}
+	return fmt.Sprintf("%s %x", str, cp.Data)
+}
---
+++ canbus/config.go
@@ -0,0 +1,173 @@
+/*************************************************************************
+ * Copyright 2017 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package main
+
+import (
+	"errors"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gravwell/ingest"
+
+	"gopkg.in/gcfg.v1"
+)
+
+const (
+	maxConfigSize  int64 = (1024 * 1024 * 2) //2MB, even this is crazy large
+	maxSnapLen     int   = 0xffff
+	defaultSnapLen int   = 96
+)
+
+type cfgType struct {
+	Global struct {
+		Ingest_Secret              string
+		Connection_Timeout         string
+		Verify_Remote_Certificates bool
+		Cleartext_Backend_Target   []string
+		Encrypted_Backend_Target   []string
+		Pipe_Backend_Target        []string
+	}
+	Sniffer map[string]*struct {
+		Interface       string //interface name to bind to
+		Tag_Name        string
+		Source_Override string
+	}
+}
+
+func GetConfig(path string) (*cfgType, error) {
+	var content []byte
+	fin, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	fi, err := fin.Stat()
+	if err != nil {
+		fin.Close()
+		return nil, err
+	}
+	//This is just a sanity check
+	if fi.Size() > maxConfigSize {
+		fin.Close()
+		return nil, errors.New("Config File Far too large")
+	}
+	content = make([]byte, fi.Size())
+	n, err := fin.Read(content)
+	fin.Close()
+	if int64(n) != fi.Size() {
+		return nil, errors.New("Failed to read config file")
+	}
+
+	var c cfgType
+	if err := gcfg.ReadStringInto(&c, string(content)); err != nil {
+		return nil, err
+	}
+	if err := verifyConfig(&c); err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
+
+func verifyConfig(c *cfgType) error {
+	if to, err := c.parseTimeout(); err != nil || to < 0 {
+		if err != nil {
+			return err
+		}
+		return errors.New("Invalid connection timeout")
+	}
+	if c.Global.Ingest_Secret == "" {
+		return errors.New("Ingest-Secret not specified")
+	}
+	//ensure there is at least one target
+	connCount := len(c.Global.Cleartext_Backend_Target) +
+		len(c.Global.Encrypted_Backend_Target) +
+		len(c.Global.Pipe_Backend_Target)
+	if connCount == 0 {
+		return errors.New("No backend targets specified")
+	}
+	if len(c.Sniffer) == 0 {
+		return errors.New("No Sniffers specified")
+	}
+	for k, v := range c.Sniffer {
+		if len(v.Interface) == 0 {
+			return errors.New("No Inteface provided for " + k)
+		}
+		if len(v.Tag_Name) == 0 {
+			v.Tag_Name = `default`
+		}
+		if strings.ContainsAny(v.Tag_Name, ingest.FORBIDDEN_TAG_SET) {
+			return errors.New("Invalid characters in the \"" + v.Tag_Name + "\"Tag-Name for " + k)
+		}
+		if v.Source_Override != `` {
+			if net.ParseIP(v.Source_Override) == nil {
+				return errors.New("Failed to parse Source_Override")
+			}
+		}
+	}
+	return nil
+}
+
+func (c *cfgType) Targets() ([]string, error) {
+	var conns []string
+	for _, v := range c.Global.Cleartext_Backend_Target {
+		conns = append(conns, "tcp://"+v)
+	}
+	for _, v := range c.Global.Encrypted_Backend_Target {
+		conns = append(conns, "tls://"+v)
+	}
+	for _, v := range c.Global.Pipe_Backend_Target {
+		conns = append(conns, "pipe://"+v)
+	}
+	if len(conns) == 0 {
+		return nil, errors.New("no connections specified")
+	}
+	return conns, nil
+}
+
+func (c *cfgType) Tags() ([]string, error) {
+	var tags []string
+	tagMp := make(map[string]bool, 1)
+	for _, v := range c.Sniffer {
+		if len(v.Tag_Name) == 0 {
+			continue
+		}
+		if _, ok := tagMp[v.Tag_Name]; !ok {
+			tags = append(tags, v.Tag_Name)
+			tagMp[v.Tag_Name] = true
+		}
+	}
+	if len(tags) == 0 {
+		return nil, errors.New("No tags specified")
+	}
+	return tags, nil
+}
+
+func (c *cfgType) VerifyRemote() bool {
+	return c.Global.Verify_Remote_Certificates
+}
+
+func (c *cfgType) Timeout() time.Duration {
+	if tos, _ := c.parseTimeout(); tos > 0 {
+		return tos
+	}
+	return 0
+}
+
+func (c *cfgType) Secret() string {
+	return c.Global.Ingest_Secret
+}
+
+func (c *cfgType) parseTimeout() (time.Duration, error) {
+	tos := strings.TrimSpace(c.Global.Connection_Timeout)
+	if len(tos) == 0 {
+		return 0, nil
+	}
+	return time.ParseDuration(tos)
+}
