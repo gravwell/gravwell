b606e9cdf82ab1298504e3a64bcc9858daa05d1d
John Floren <john@jfloren.net>
2019-10-09 17:53:46 -0600

    tweak to kinesis nil iterator handling

--- KinesisIngester/main.go
+++ KinesisIngester/main.go
@@ -191,6 +191,11 @@ func main() {
 					lg.Error("error on shard #%d (%s): %v", shardid, *shard.ShardId, err)
 					return
 				}
+				if output.ShardIterator == nil {
+					// this is weird, we are going to bail out
+					lg.Error("Got nil initial shard iterator, bailing out")
+					return
+				}
 				iter := *output.ShardIterator
 				eChan := make(chan *entry.Entry, 2048)
 				tcfg := timegrinder.Config{
@@ -251,6 +256,10 @@ func main() {
 						}
 					}
 
+					if res.NextShardIterator == nil {
+						// Hmm. Skip and try again.
+						continue
+					}
 					iter = *res.NextShardIterator
 					for _, r := range res.Records {
 						ent := &entry.Entry{
