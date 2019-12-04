a4dc194d31fc64e421685166582e2968d5cc3946
John Floren <john@jfloren.net>
2019-12-03 17:32:57 -0700

    argh, update version

--- version/version.go
+++ version/version.go
@@ -17,11 +17,11 @@ import (
 const (
 	MajorVersion = 3
 	MinorVersion = 3
-	PointVersion = 0
+	PointVersion = 1
 )
 
 var (
-	BuildDate time.Time = time.Date(2019, 11, 11, 0, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2019, 12, 03, 0, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
