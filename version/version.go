069c2f581d99ae727cfe603188bdf5b0b8a1a3f6
John Floren <john@jfloren.net>
2019-12-31 10:21:54 -0700

    update build date

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ const (
 )
 
 var (
-	BuildDate time.Time = time.Date(2019, 12, 23, 0, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2019, 12, 31, 0, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
