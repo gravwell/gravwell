d1737a60e64e4668fe89eff2b3c9d28a1a4e81cb
Kris Watts <kris.watts@gravwell.io>
2020-02-04 14:22:40 -0700

    updating the extraction code to support some additional interfaces and resettting the reader

--- utils/extract.go
+++ utils/extract.go
@@ -12,6 +12,7 @@ import (
 	"bufio"
 	"compress/bzip2"
 	"compress/gzip"
+	"errors"
 	"io"
 	"os"
 
@@ -23,9 +24,16 @@ const (
 	defaultBufferSize int = 2 * 1024 * 1024
 )
 
+type ReadResetCloser interface {
+	Read([]byte) (int, error)
+	Close() error
+	Reset() error
+}
+
 type buffReadCloser struct {
-	r io.ReadCloser
-	b io.Reader
+	r     ReadResetCloser
+	b     io.Reader
+	bsize int
 }
 
 func (brc *buffReadCloser) Read(x []byte) (int, error) {
@@ -39,20 +47,29 @@ func (brc *buffReadCloser) Close() error {
 	return brc.r.Close()
 }
 
-func OpenBufferedFileReader(p string, buffer int) (r io.ReadCloser, err error) {
+func (brc *buffReadCloser) Reset() (err error) {
+	if err = brc.r.Reset(); err != nil {
+		return
+	}
+	brc.b = bufio.NewReaderSize(brc.r, brc.bsize)
+	return
+}
+
+func OpenBufferedFileReader(p string, buffer int) (r ReadResetCloser, err error) {
 	if buffer <= 0 {
 		buffer = defaultBufferSize
 	}
 	if r, err = OpenFileReader(p); err == nil {
 		r = &buffReadCloser{
-			r: r,
-			b: bufio.NewReaderSize(r, buffer),
+			r:     r,
+			b:     bufio.NewReaderSize(r, buffer),
+			bsize: buffer,
 		}
 	}
 	return
 }
 
-func OpenFileReader(p string) (r io.ReadCloser, err error) {
+func OpenFileReader(p string) (r ReadResetCloser, err error) {
 	var fin *os.File
 	var tp types.Type
 	if tp, err = ft.MatchFile(p); err != nil {
@@ -67,34 +84,89 @@ func OpenFileReader(p string) (r io.ReadCloser, err error) {
 	return
 }
 
-func getReader(fin *os.File, tp types.Type) (r io.ReadCloser, err error) {
+func getReader(fin *os.File, tp types.Type) (r ReadResetCloser, err error) {
 	switch tp.MIME.Subtype {
 	case `gzip`:
-		r, err = gzip.NewReader(fin)
+		r, err = newGzipReader(newFileReadResetCloser(fin))
 	case `x-bzip2`:
-		r = newReadCloser(fin, bzip2.NewReader(fin))
+		r, err = newBzip2Reader(newFileReadResetCloser(fin))
 	default:
-		r = fin
+		r = newFileReadResetCloser(fin)
 	}
 	return
 }
 
-type rc struct {
-	fin *os.File
+type fileResetter struct {
+	*os.File
+}
+
+func newFileReadResetCloser(fin *os.File) ReadResetCloser {
+	return fileResetter{File: fin}
+}
+
+func (fr fileResetter) Reset() (err error) {
+	var n int64
+	if n, err = fr.Seek(0, 0); err != nil {
+		return
+	} else if n != 0 {
+		err = errors.New("Failed to seek")
+	}
+	return
+}
+
+type gzipReader struct {
+	fin ReadResetCloser
+	rdr *gzip.Reader
+}
+
+func newGzipReader(rdr ReadResetCloser) (gzr *gzipReader, err error) {
+	gzr = &gzipReader{
+		fin: rdr,
+	}
+	gzr.rdr, err = gzip.NewReader(gzr.fin)
+	return
+}
+
+func (gr *gzipReader) Read(b []byte) (int, error) {
+	return gr.rdr.Read(b)
+}
+
+func (gr *gzipReader) Close() error {
+	return gr.rdr.Close()
+}
+
+func (gr *gzipReader) Reset() (err error) {
+	if err = gr.fin.Reset(); err != nil {
+		return
+	}
+	gr.rdr, err = gzip.NewReader(gr.fin)
+	return
+}
+
+type bzip2Reader struct {
+	fin ReadResetCloser
 	rdr io.Reader
 }
 
-func newReadCloser(fin *os.File, rdr io.Reader) io.ReadCloser {
-	return &rc{
-		fin: fin,
-		rdr: rdr,
+func newBzip2Reader(rdr ReadResetCloser) (bzr *bzip2Reader, err error) {
+	bzr = &bzip2Reader{
+		fin: rdr,
+		rdr: bzip2.NewReader(rdr),
 	}
+	return
 }
 
-func (r *rc) Close() error {
-	return r.fin.Close()
+func (bzr *bzip2Reader) Read(b []byte) (int, error) {
+	return bzr.rdr.Read(b)
 }
 
-func (r *rc) Read(b []byte) (int, error) {
-	return r.rdr.Read(b)
+func (bzr *bzip2Reader) Close() error {
+	return bzr.fin.Close()
+}
+
+func (bzr *bzip2Reader) Reset() (err error) {
+	if err = bzr.fin.Reset(); err == nil {
+		bzr.rdr = bzip2.NewReader(bzr.fin)
+	}
+	return
 }
