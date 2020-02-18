ae1dfeab66727c636c9803d006d5e0e29f1d6631
Kris Watts <kris.watts@gravwell.io>
2020-02-18 09:32:26 -0700

    adding handlers for the stderr logger for windows and linux

--- log/logging.go
+++ log/logging.go
@@ -17,7 +17,6 @@ import (
 	"runtime"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 )
 
@@ -324,32 +323,3 @@ func NewStderrLoggerEx(fileOverride string, cb StderrCallback) (*Logger, error) 
 }
 
 type StderrCallback func(io.Writer)
-
-func newStderrLogger(fileOverride string, cb StderrCallback) (lgr *Logger, err error) {
-	var oldstderr int
-	var fout *os.File
-	lgr = New(os.Stderr)
-	if len(fileOverride) > 0 {
-		//get a handle on the output file
-		if fout, err = os.Create(fileOverride); err != nil {
-			return
-		}
-		if cb != nil {
-			cb(fout)
-		}
-
-		//dup stderr
-		if oldstderr, err = syscall.Dup(int(os.Stderr.Fd())); err != nil {
-			fout.Close()
-			return
-		} else {
-			lgr.AddWriter(os.NewFile(uintptr(oldstderr), "oldstderr"))
-		}
-
-		//dupe the output file onto stderr so that output goes there
-		if err = syscall.Dup2(int(fout.Fd()), int(os.Stderr.Fd())); err != nil {
-			fout.Close()
-		}
-	}
-	return
-}
