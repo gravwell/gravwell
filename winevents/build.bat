6f0117a55510ca0762edba415ccd2ff3b1681eb3
Oto Ciulis <oto.ciulis@gmail.com>
2020-03-03 15:52:13 -0800

    Adding MSI localization & configuration dialog

--- winevents/build.bat
+++ winevents/build.bat
@@ -1,2 +1,2 @@
-candle -arch x64 LicenseAgreementDlg_HK.wxs WixUI_HK.wxs product.wxs
-light -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out gravwell_win_events_3.2.8.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj product.wixobj
+candle -arch x64 LicenseAgreementDlg_HK.wxs ConfigurationDlg.wxs WixUI_HK.wxs product.wxs
+light -cultures:en-us -loc en-us.wxl -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out gravwell_win_events_3.2.8.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj product.wixobj ConfigurationDlg.wixobj
