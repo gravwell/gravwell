17f99eaa061b164105ecae3e424a641063bcc17b
Oto Ciulis <oto.ciulis@gmail.com>
2020-03-09 10:04:08 -0700

    Fixing reported issues

--- winevents/build.bat
+++ winevents/build.bat
@@ -1,2 +1,2 @@
-candle -arch x64 LicenseAgreementDlg_HK.wxs ConfigurationDlg.wxs WixUI_HK.wxs product.wxs
+candle -arch x64 LicenseAgreementDlg_HK.wxs ConfigurationDlg.wxs WixUI_HK.wxs product.wxs -dVersion=3.2.8
 light -cultures:en-us -loc en-us.wxl -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out gravwell_win_events_3.2.8.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj product.wixobj ConfigurationDlg.wixobj
