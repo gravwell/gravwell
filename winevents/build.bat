fb8629ef5e4183d1201e278d218043b3d6f4d898
Oto Ciulis <oto.ciulis@gmail.com>
2020-03-11 15:58:35 -0700

    Fixing min version of the upgrade sequence

--- winevents/build.bat
+++ winevents/build.bat
@@ -1,2 +1,3 @@
-candle -arch x64 LicenseAgreementDlg_HK.wxs ConfigurationDlg.wxs WixUI_HK.wxs product.wxs -dVersion=3.2.8
-light -cultures:en-us -loc en-us.wxl -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out gravwell_win_events_3.2.8.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj product.wixobj ConfigurationDlg.wixobj
+set VERSION=3.2.8
+candle -arch x64 LicenseAgreementDlg_HK.wxs ConfigurationDlg.wxs WixUI_HK.wxs product.wxs -dVersion=%VERSION%
+light -cultures:en-us -loc en-us.wxl -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out gravwell_win_events_%VERSION%.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj product.wixobj ConfigurationDlg.wixobj
