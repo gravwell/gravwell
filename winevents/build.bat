325771a81aedcf2ab1388897b759545c22105d38
Kris Watts <kris.watts@gravwell.io>
2020-03-12 17:01:30 -0600

    bumping version to test upgrades

--- winevents/build.bat
+++ winevents/build.bat
@@ -1,3 +1,3 @@
-set VERSION=3.2.9
+set VERSION=3.2.10
 candle -arch x64 LicenseAgreementDlg_HK.wxs ConfigurationDlg.wxs WixUI_HK.wxs product.wxs -dVersion=%VERSION%
 light -cultures:en-us -loc en-us.wxl -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out gravwell_win_events_%VERSION%.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj product.wixobj ConfigurationDlg.wixobj
