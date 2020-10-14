74bb94ec0c68b8cd81c1d93a8796a774c1ce3abb
David Fritz <fritz@gravwell.io>
2020-10-14 12:01:21 -0600

    remove unneeded logging message that causes a deadlock.
    fixes #94

--- ingest/muxer.go
+++ ingest/muxer.go
@@ -536,7 +536,6 @@ func (im *IngestMuxer) NegotiateTag(name string) (tg entry.EntryTag, err error) 
 			remoteTag, err := v.NegotiateTag(name)
 			if err != nil {
 				// something went wrong, kill it and let it re-initialize
-				im.Error("NegotiateTag on %v for %v: %v", v.conn.RemoteAddr(), name, err)
 				v.Close()
 				continue
 			}
