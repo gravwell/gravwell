9237c936d1db35280a1623a1e1893c538faffa47
John Floren <john@jfloren.net>
2021-04-15 11:09:41 -0700

    reduce code duplication

--- ingest/processors/processors_linux.go
+++ ingest/processors/processors_linux.go
@@ -14,22 +14,8 @@ import (
 	"github.com/gravwell/gravwell/v3/ingest/config"
 )
 
-func CheckProcessor(id string) error {
-	id = strings.TrimSpace(strings.ToLower(id))
+func checkProcessorOS(id string) error {
 	switch id {
-	case GzipProcessor:
-	case JsonExtractProcessor:
-	case JsonArraySplitProcessor:
-	case JsonFilterProcessor:
-	case RegexTimestampProcessor:
-	case RegexExtractProcessor:
-	case RegexRouterProcessor:
-	case ForwarderProcessor:
-	case VpcProcessor:
-	case GravwellForwarderProcessor:
-	case DropProcessor:
-	case CiscoISEProcessor:
-	case SrcRouterProcessor:
 	case PersistentBufferProcessor:
 	default:
 		return ErrUnknownProcessor
@@ -37,38 +23,12 @@ func CheckProcessor(id string) error {
 	return nil
 }
 
-func ProcessorLoadConfig(vc *config.VariableConfig) (cfg interface{}, err error) {
+func processorLoadConfigOS(vc *config.VariableConfig) (cfg interface{}, err error) {
 	var pb preprocessorBase
 	if err = vc.MapTo(&pb); err != nil {
 		return
 	}
 	switch strings.TrimSpace(strings.ToLower(pb.Type)) {
-	case DropProcessor:
-		cfg, err = DropLoadConfig(vc)
-	case GzipProcessor:
-		cfg, err = GzipLoadConfig(vc)
-	case JsonExtractProcessor:
-		cfg, err = JsonExtractLoadConfig(vc)
-	case JsonArraySplitProcessor:
-		cfg, err = JsonArraySplitLoadConfig(vc)
-	case JsonFilterProcessor:
-		cfg, err = JsonFilterLoadConfig(vc)
-	case RegexTimestampProcessor:
-		cfg, err = RegexTimestampLoadConfig(vc)
-	case RegexExtractProcessor:
-		cfg, err = RegexExtractLoadConfig(vc)
-	case RegexRouterProcessor:
-		cfg, err = RegexRouteLoadConfig(vc)
-	case ForwarderProcessor:
-		cfg, err = ForwarderLoadConfig(vc)
-	case VpcProcessor:
-		cfg, err = VpcLoadConfig(vc)
-	case GravwellForwarderProcessor:
-		cfg, err = GravwellForwarderLoadConfig(vc)
-	case CiscoISEProcessor:
-		cfg, err = CiscoISELoadConfig(vc)
-	case SrcRouterProcessor:
-		cfg, err = SrcRouteLoadConfig(vc)
 	case PersistentBufferProcessor:
 		cfg, err = PersistentBufferLoadConfig(vc)
 	default:
@@ -77,91 +37,13 @@ func ProcessorLoadConfig(vc *config.VariableConfig) (cfg interface{}, err error)
 	return
 }
 
-func newProcessor(vc *config.VariableConfig, tgr Tagger) (p Processor, err error) {
+func newProcessorOS(vc *config.VariableConfig, tgr Tagger) (p Processor, err error) {
 	var pb preprocessorBase
 	if err = vc.MapTo(&pb); err != nil {
 		return
 	}
 	id := strings.TrimSpace(strings.ToLower(pb.Type))
 	switch id {
-	case DropProcessor:
-		var cfg DropConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewDrop(cfg)
-	case GzipProcessor:
-		var cfg GzipDecompressorConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewGzipDecompressor(cfg)
-	case JsonExtractProcessor:
-		var cfg JsonExtractConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewJsonExtractor(cfg)
-	case JsonArraySplitProcessor:
-		var cfg JsonArraySplitConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewJsonArraySplitter(cfg)
-	case JsonFilterProcessor:
-		var cfg JsonFilterConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewJsonFilter(cfg)
-	case RegexTimestampProcessor:
-		var cfg RegexTimestampConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewRegexTimestampProcessor(cfg)
-	case RegexExtractProcessor:
-		var cfg RegexExtractConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewRegexExtractor(cfg)
-	case RegexRouterProcessor:
-		var cfg RegexRouteConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewRegexRouter(cfg, tgr)
-	case ForwarderProcessor:
-		var cfg ForwarderConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewForwarder(cfg, tgr)
-	case VpcProcessor:
-		var cfg VpcConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewVpcProcessor(cfg)
-	case GravwellForwarderProcessor:
-		var cfg GravwellForwarderConfig
-		if cfg, err = GravwellForwarderLoadConfig(vc); err != nil {
-			return
-		}
-		p, err = NewGravwellForwarder(cfg, tgr)
-	case CiscoISEProcessor:
-		var cfg CiscoISEConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewCiscoISEProcessor(cfg)
-	case SrcRouterProcessor:
-		var cfg SrcRouteConfig
-		if err = vc.MapTo(&cfg); err != nil {
-			return
-		}
-		p, err = NewSrcRouter(cfg, tgr)
 	case PersistentBufferProcessor:
 		var cfg PersistentBufferConfig
 		if err = vc.MapTo(&cfg); err != nil {
