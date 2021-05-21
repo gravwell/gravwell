ed6ed3e5fdfe0f74ef12b4e148b94051834b58bf
kristopher watts <kris.watts@gravwell.io>
2021-05-21 09:51:06 -0600

    converting logging system over to RFC5424

--- ingest/log/logging.go
+++ ingest/log/logging.go
@@ -18,6 +18,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/crewjam/rfc5424"
 )
 
 const (
@@ -27,10 +29,17 @@ const (
 	WARN     Level = 3
 	ERROR    Level = 4
 	CRITICAL Level = 5
+	FATAL    Level = 6
 )
 
 const (
 	DEFAULT_DEPTH = 3
+
+	defaultID = `gw@1`
+
+	maxProcID   = 128
+	maxAppname  = 48
+	maxHostname = 255
 )
 
 var (
@@ -40,11 +49,59 @@ var (
 
 type Level int
 
+type metadata struct {
+	hostname string
+	appname  string
+}
+
+func (m *metadata) SetHostname(hostname string) error {
+	if hostname != `` {
+		if err := checkName(hostname); err != nil {
+			return err
+		}
+	}
+	if m.hostname = hostname; m.hostname == `` {
+		//try to grab it via os package
+		var lerr error
+		if m.hostname, lerr = os.Hostname(); lerr == nil {
+			if len(m.hostname) > maxHostname {
+				m.hostname = m.hostname[0:maxHostname]
+			}
+		}
+	}
+	return nil
+}
+
+func (m *metadata) SetAppname(appname string) error {
+	if appname != `` {
+		if err := checkName(appname); err != nil {
+			return err
+		}
+	}
+	if m.appname = appname; len(m.appname) > maxAppname {
+		m.appname = m.appname[0:maxAppname]
+	}
+	return nil
+}
+
+func (m *metadata) guessHostnameAppname() {
+	m.SetHostname(``)
+	if args := os.Args; len(args) > 0 {
+		exe := filepath.Base(args[0])
+		if ext := filepath.Ext(exe); len(ext) > 0 && len(ext) < len(exe) {
+			exe = strings.TrimSuffix(exe, ext)
+		}
+		m.SetAppname(exe)
+	}
+}
+
 type Logger struct {
+	metadata
 	wtrs []io.WriteCloser
 	mtx  sync.Mutex
 	lvl  Level
 	hot  bool
+	raw  bool //output the old raw form rather than RFC5424
 }
 
 // NewFile creates a new logger with the first writer being a file
@@ -59,13 +116,15 @@ func NewFile(f string) (*Logger, error) {
 }
 
 // New creates a new logger with the given writer at log level INFO
-func New(wtr io.WriteCloser) *Logger {
-	return &Logger{
+func New(wtr io.WriteCloser) (l *Logger) {
+	l = &Logger{
 		wtrs: []io.WriteCloser{wtr},
 		mtx:  sync.Mutex{},
 		lvl:  INFO,
 		hot:  true,
 	}
+	l.guessHostnameAppname()
+	return
 }
 
 func NewDiscardLogger() *Logger {
@@ -88,6 +147,10 @@ func (l *Logger) Close() (err error) {
 		}
 	}
 	return
+}
+
+func (l *Logger) EnableRawMode() {
+	l.raw = true //no need for a mutex here
 }
 
 func (l *Logger) ready() error {
@@ -234,15 +297,23 @@ func (l *Logger) FatalCode(code int, f string, args ...interface{}) {
 	l.fatalCode(DEFAULT_DEPTH, code, f, args...)
 }
 
-func (l *Logger) fatalCode(lvl, code int, f string, args ...interface{}) {
-	var nl string
-	if !strings.HasSuffix(f, "\n") {
-		nl = "\n"
+func (l *Logger) fatalCode(depth, code int, f string, args ...interface{}) {
+	var ln string
+	var nlReq bool
+	if l.raw {
+		ln = prefix(depth) + " FATAL " + fmt.Sprintf(f, args...)
+	} else {
+		ln = l.genRfcOutput(callLoc(depth), FATAL, fmt.Sprintf(f, args...))
 	}
-	ln := prefix(lvl) + " FATAL " + fmt.Sprintf(f, args...) + nl
+	if !strings.HasPrefix(ln, "\n") {
+		nlReq = true
+	}
 	l.mtx.Lock()
 	for _, w := range l.wtrs {
 		io.WriteString(w, ln)
+		if nlReq {
+			io.WriteString(w, "\n")
+		}
 		w.Close()
 	}
 	os.Exit(code)
@@ -250,21 +321,56 @@ func (l *Logger) fatalCode(lvl, code int, f string, args ...interface{}) {
 }
 
 func (l *Logger) output(depth int, lvl Level, f string, args ...interface{}) (err error) {
+	var nlReq bool
 	l.mtx.Lock()
 	if err = l.ready(); err == nil && l.lvl <= lvl && l.lvl != OFF {
-		var nl string
-		if !strings.HasSuffix(f, "\n") {
-			nl = "\n"
+		ln := l.genOutput(callLoc(depth), lvl, f, args...)
+		if !strings.HasPrefix(ln, "\n") {
+			nlReq = true
 		}
-		ln := prefix(depth) + " " + lvl.String() + " " + fmt.Sprintf(f, args...) + nl
 		for _, w := range l.wtrs {
 			if _, lerr := io.WriteString(w, ln); lerr != nil {
 				err = lerr
+			} else if nlReq {
+				if _, lerr = io.WriteString(w, "\n"); lerr != nil {
+					err = lerr
+				}
 			}
 		}
 	}
 	l.mtx.Unlock()
 	return
+}
+
+func (l *Logger) genOutput(pfx string, lvl Level, f string, args ...interface{}) (ln string) {
+	if l.raw {
+		return l.genRawOutput(pfx, lvl, f, args...)
+	}
+	return l.genRfcOutput(pfx, lvl, fmt.Sprintf(f, args...))
+}
+
+func (l *Logger) genRfcOutput(pfx string, lvl Level, payload string) (ln string) {
+	m := rfc5424.Message{
+		Priority:  lvl.priority(),
+		Timestamp: time.Now(),
+		Hostname:  l.hostname,
+		AppName:   l.appname,
+		MessageID: pfx,
+		Message:   []byte(payload),
+	}
+	if b, err := m.MarshalBinary(); err == nil {
+		ln = string(b)
+	}
+	return
+}
+
+func (l *Logger) genRawOutput(pfx string, lvl Level, f string, args ...interface{}) string {
+	var nl string
+	if !strings.HasSuffix(f, "\n") {
+		nl = "\n"
+	}
+	ts := time.Now().UTC().Format(time.RFC3339)
+	return ts + " " + pfx + " " + lvl.String() + " " + fmt.Sprintf(f, args...) + nl
 }
 
 // implement writer interface so it can be handed to a standard loger
@@ -297,6 +403,8 @@ func (l Level) String() string {
 		return `ERROR`
 	case CRITICAL:
 		return `CRITICAL`
+	case FATAL:
+		return `FATAL`
 	}
 	return `UNKNOWN`
 }
@@ -314,9 +422,31 @@ func (l Level) Valid() bool {
 	case ERROR:
 		fallthrough
 	case CRITICAL:
+		fallthrough
+	case FATAL:
 		return true
 	}
 	return false
+}
+
+func (l Level) priority() rfc5424.Priority {
+	switch l {
+	case OFF:
+		return 0
+	case DEBUG:
+		return rfc5424.Debug
+	case INFO:
+		return rfc5424.Info
+	case WARN:
+		return rfc5424.Warning
+	case ERROR:
+		return rfc5424.Error
+	case CRITICAL:
+		return rfc5424.Crit
+	case FATAL:
+		return rfc5424.Emergency
+	}
+	return rfc5424.Debug
 }
 
 func LevelFromString(s string) (l Level, err error) {
@@ -334,6 +464,8 @@ func LevelFromString(s string) (l Level, err error) {
 		l = ERROR
 	case `CRITICAL`:
 		l = CRITICAL
+	case `FATAL`:
+		l = FATAL
 	default:
 		err = ErrInvalidLevel
 	}
@@ -364,6 +496,16 @@ func prefix(callDepth int) (s string) {
 	return
 }
 
+func callLoc(callDepth int) (s string) {
+	//get the file and line that caused the error
+	if _, file, line, ok := runtime.Caller(callDepth); ok {
+		dir, file := filepath.Split(file)
+		file = filepath.Join(filepath.Base(dir), file)
+		s = fmt.Sprintf("%s:%d", file, line)
+	}
+	return
+}
+
 func NewStderrLogger(fileOverride string) (*Logger, error) {
 	return newStderrLogger(fileOverride, nil)
 }
@@ -373,3 +515,20 @@ func NewStderrLoggerEx(fileOverride string, cb StderrCallback) (*Logger, error) 
 }
 
 type StderrCallback func(io.Writer)
+
+func checkName(v string) (err error) {
+	//check that bits[0] only contains ascii values
+	for _, r := range v {
+		//must be a-z or A-Z, or . _, -
+		if r >= 'a' && r <= 'z' {
+			continue
+		} else if r >= 'A' && r <= 'Z' {
+			continue
+		} else if r == '.' || r == '_' || r == '-' || r == ':' {
+			continue
+		}
+		err = fmt.Errorf("name character %c is invalid", r)
+		return
+	}
+	return
+}
