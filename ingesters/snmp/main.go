8d1873d2c284bbe8c016d50a6546b9724be210f3
John Floren <john@jfloren.net>
2023-02-16 13:57:52 -0800

    Drop v2 messages with the wrong community

--- ingesters/snmp/main.go
+++ ingesters/snmp/main.go
@@ -129,6 +129,12 @@ func main() {
 					log.KV("expected-flags", 0x3&l.Params.MsgFlags),
 					log.KV("client", u.IP.String()))
 				return
+			} else if l.Params.Version == gosnmp.Version2c && s.Community != l.Params.Community {
+				ib.Logger.Warn("dropping trap due to invalid community",
+					log.KV("received-community", s.Community),
+					log.KV("received-community", l.Params.Community),
+					log.KV("client", u.IP.String()))
+				return
 			}
 			ent := entry.Entry{
 				TS:  entry.Now(),
