9eaabee4cf5fbaff6675b4eddc4e69874d34addf
kris <kris.watts@gravwell.io>
2023-02-23 08:13:36 -0700

    bumping version for monorepo

--- ingesters/version/version.go
+++ ingesters/version/version.go
@@ -17,11 +17,11 @@ import (
 const (
 	MajorVersion = 5
 	MinorVersion = 2
-	PointVersion = 0
+	PointVersion = 1
 )
 
 var (
-	BuildDate time.Time = time.Date(2023, 01, 31, 0, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2023, 03, 16, 0, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
