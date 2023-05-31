7e6da1013a67c6ab8789ad99098e5d9947a69c38
John Floren <john@jfloren.net>
2023-05-31 14:02:00 -0700

    typo

--- client/urls.go
+++ client/urls.go
@@ -149,7 +149,7 @@ const (
 	CAPABILITY_LIST_URL              = `/api/info/capabilities`
 	CAPABILITY_TEMPLATE_LIST_URL     = `/api/info/capabilities/templates`
 	CAPABILITY_CURRENT_USER_LIST_URL = `/api/info/capabilities/my`
-	CAPABILITY_CURRENT_USER_WHY_URL  = `/api/info/capabilities/whyy`
+	CAPABILITY_CURRENT_USER_WHY_URL  = `/api/info/capabilities/why`
 	CAPABILITY_USER_URL              = `/api/users/%d/capabilities`
 	CAPABILITY_USER_WHY_URL          = `/api/users/%d/capabilities`
 	CAPABILITY_GROUP_URL             = `/api/groups/%d/capabilities`
