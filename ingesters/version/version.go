7215d08c320cb7f11b4554ab6e1189982f41a170
kris-watts-gravwell <kris.watts@gravwell.io>
2023-07-20 21:02:04 -0600

    updating version for 5.3.3 spot release

--- ingesters/version/version.go
+++ ingesters/version/version.go
@@ -17,11 +17,11 @@ import (
 const (
 	MajorVersion = 5
 	MinorVersion = 3
-	PointVersion = 2
+	PointVersion = 3
 )
 
 var (
-	BuildDate time.Time = time.Date(2023, 6, 29, 0, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2023, 7, 21, 0, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
