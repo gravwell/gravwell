083eddb7aa00d267b95db48265278f0a6b5a73ef
kris-watts-gravwell <kris.watts@gravwell.io>
2023-08-01 11:12:22 -0600

    converting MSGraph ingester to boilerplate code

--- ingesters/MSGraphIngester/config.go
+++ ingesters/MSGraphIngester/config.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/gravwell/gravwell/v3/ingest/attach"
 	"github.com/gravwell/gravwell/v3/ingest/config"
 	"github.com/gravwell/gravwell/v3/ingest/processors"
 )
@@ -48,6 +49,7 @@ type contentType struct {
 
 type cfgType struct {
 	Global       global
+	Attach       attach.AttachConfig
 	ContentType  map[string]*contentType
 	Preprocessor processors.ProcessorConfig
 	TimeFormat   config.CustomTimeFormat
@@ -77,6 +79,12 @@ func GetConfig(path, overlayPath string) (*cfgType, error) {
 }
 
 func verifyConfig(c cfgType) error {
+	if err := c.Global.Verify(); err != nil {
+		return err
+	} else if err = c.Attach.Verify(); err != nil {
+		return err
+	}
+
 	if to, err := c.parseTimeout(); err != nil || to < 0 {
 		if err != nil {
 			return err
@@ -132,6 +140,14 @@ func (c *cfgType) Tags() ([]string, error) {
 		return nil, errors.New("No tags specified")
 	}
 	return tags, nil
+}
+
+func (c *cfgType) IngestBaseConfig() config.IngestConfig {
+	return c.Global.IngestConfig
+}
+
+func (c *cfgType) AttachConfig() attach.AttachConfig {
+	return c.Attach
 }
 
 func (c *cfgType) ContentTypes() (ret []string) {
