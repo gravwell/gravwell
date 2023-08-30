013f3c3e228e05677f0a3bd2557c32b878a31bc1
David Fritz <fritz@gravwell.io>
2023-08-30 12:36:38 -0600

    add SQS-S3-Listener to s3 ingester

--- ingesters/s3Ingester/bucket.go
+++ ingesters/s3Ingester/bucket.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"net"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/buger/jsonparser"
 	"github.com/gravwell/gravwell/v3/ingest/entry"
 	"github.com/gravwell/gravwell/v3/ingest/log"
 	"github.com/gravwell/gravwell/v3/ingest/processors"
@@ -152,28 +147,7 @@ func (br *BucketReader) ShouldTrack(obj string) (ok bool) {
 
 // Process reads the object in and processes its contents
 func (br *BucketReader) Process(obj *s3.Object) (err error) {
-	return br.ProcessContext(obj, nil)
-}
-
-func (br *BucketReader) ProcessContext(obj *s3.Object, ctx context.Context) (err error) {
-	var r *s3.GetObjectOutput
-	r, err = br.svc.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(br.Bucket_Name),
-		Key:    obj.Key,
-	})
-	if err != nil {
-		return
-	}
-	defer r.Body.Close()
-	switch br.rdr {
-	case lineReader:
-		err = br.processLinesContext(ctx, r.Body)
-	case cloudtrailReader:
-		err = br.processCloudtrailContext(ctx, r.Body)
-	default:
-		err = errors.New("no reader set")
-	}
-	return
+	return ProcessContext(obj, nil, br.svc, br.Bucket_Name, br.rdr, br.TG, br.src, br.Tag, br.Proc, br.MaxLineSize)
 }
 
 func (br *BucketReader) ManualScan(ctx context.Context, ot *objectTracker) (err error) {
@@ -229,103 +203,6 @@ func (br *BucketReader) ManualScan(ctx context.Context, ot *objectTracker) (err 
 
 	if err = br.svc.ListObjectsV2Pages(&req, objListHandler); err == nil {
 		err = lerr
-	}
-	return
-}
-
-func (br *BucketReader) processLinesContext(ctx context.Context, rdr io.Reader) (err error) {
-	sc := bufio.NewScanner(rdr)
-	sc.Buffer(nil, br.MaxLineSize)
-	for sc.Scan() {
-		bts := sc.Bytes()
-		if len(bts) == 0 {
-			continue
-		}
-		ts, ok, _ := br.TG.Extract(bts)
-		if !ok {
-			ts = time.Now()
-		}
-		ent := entry.Entry{
-			TS:   entry.FromStandard(ts),
-			SRC:  br.src,                      //may be nil, ingest muxer will handle if it is
-			Data: append([]byte(nil), bts...), //scanner re-uses the buffer
-			Tag:  br.Tag,
-		}
-		if ctx != nil {
-			err = br.Proc.ProcessContext(&ent, ctx)
-		} else {
-			err = br.Proc.Process(&ent)
-		}
-		if err != nil {
-			return //just leave
-		}
-	}
-
-	return
-}
-
-func (br *BucketReader) processCloudtrailContext(ctx context.Context, rdr io.Reader) (err error) {
-	var obj json.RawMessage
-	dec := json.NewDecoder(rdr)
-
-	var cberr error
-	cb := func(val []byte, vt jsonparser.ValueType, off int, lerr error) {
-		if lerr != nil {
-			cberr = lerr
-			return
-		}
-		var bts []byte
-		// if our record is an object try to grab a handle on the eventTime member
-		// if not, just take the whole thing, this is an optimization to process timestamps
-		if vt == jsonparser.Object {
-			if eventTime, err := jsonparser.GetString(val, `eventTime`); err == nil {
-				bts = []byte(eventTime)
-			} else {
-				bts = val // could not match, just set to whole thing and let TG do its thing
-			}
-		} else {
-			bts = val
-		}
-		ts, ok, _ := br.TG.Extract(bts)
-		if !ok {
-			ts = time.Now()
-		}
-		ent := entry.Entry{
-			TS:   entry.FromStandard(ts),
-			SRC:  br.src,                      //may be nil, ingest muxer will handle if it is
-			Data: append([]byte(nil), val...), //scanner re-uses the buffer
-			Tag:  br.Tag,
-		}
-		if ctx != nil {
-			cberr = br.Proc.ProcessContext(&ent, ctx)
-		} else {
-			cberr = br.Proc.Process(&ent)
-		}
-		return
-	}
-
-	for {
-		var recordarray []byte
-		var dt jsonparser.ValueType
-		if err = dec.Decode(&obj); err != nil {
-			if err == io.EOF {
-				err = nil
-			}
-			break
-		}
-		if recordarray, dt, _, err = jsonparser.Get([]byte(obj), `Records`); err != nil {
-			err = fmt.Errorf("failed to find Records array in cloudtrail log: %v", err)
-			break
-		} else if dt != jsonparser.Array {
-			err = fmt.Errorf("Records member is an invalid type: %v", dt)
-			break
-		}
-		if _, err = jsonparser.ArrayEach(recordarray, cb); err != nil {
-			break
-		} else if cberr != nil {
-			err = cberr
-			break
-		}
 	}
 	return
 }
