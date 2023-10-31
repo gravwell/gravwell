683fa20f3cedca8eec16c152ee8b5636099f1157
kris-watts-gravwell <kris.watts@gravwell.io>
2023-10-30 19:11:29 -0600

    adjusting limited reader

--- ingesters/HttpIngester/handlers.go
+++ ingesters/HttpIngester/handlers.go
@@ -317,13 +317,14 @@ func handleMulti(h *handler, cfg routeHandler, w http.ResponseWriter, r *http.Re
 }
 
 func handleSingle(h *handler, cfg routeHandler, w http.ResponseWriter, r *http.Request, rdr io.Reader, ip net.IP) {
-	b, err := ioutil.ReadAll(&io.LimitedReader{R: rdr, N: int64(maxBody + 1)})
+	lr := io.LimitedReader{R: rdr, N: int64(maxBody + 1)}
+	b, err := ioutil.ReadAll(&lr)
 	if err != nil && err != io.EOF {
 		h.lgr.Info("got bad request", log.KV("address", ip), log.KVErr(err))
 		w.WriteHeader(http.StatusBadRequest)
 		return
-	} else if len(b) > maxBody {
-		h.lgr.Error("request too large, 4MB max")
+	} else if len(b) > maxBody || lr.N == 0 {
+		h.lgr.Error("request too large", log.KV("max", maxBody))
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
