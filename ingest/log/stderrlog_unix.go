810eab7d987b2170d284cdd92c6a0838b9c486ee
kris-watts-gravwell <kris.watts@gravwell.io>
2023-11-17 07:16:47 -0700

    make logging be way less chatty and dumb on stderr

--- ingest/log/stderrlog_unix.go
+++ ingest/log/stderrlog_unix.go
@@ -12,15 +12,17 @@
 package log
 
 import (
+	"io"
 	"os"
 	"syscall"
+	"time"
 )
 
 func newStderrLogger(fileOverride string, cb StderrCallback) (lgr *Logger, err error) {
-	var oldstderr int
-	var fout *os.File
-	lgr = New(os.Stderr)
+	var clr critLevelRelay
 	if len(fileOverride) > 0 {
+		var oldstderr int
+		var fout *os.File
 		//get a handle on the output file
 		if fout, err = os.Create(fileOverride); err != nil {
 			return
@@ -34,12 +36,40 @@ func newStderrLogger(fileOverride string, cb StderrCallback) (lgr *Logger, err e
 			fout.Close()
 			return
 		} else {
-			lgr.AddWriter(os.NewFile(uintptr(oldstderr), "oldstderr"))
+			clr.wc = []io.WriteCloser{os.NewFile(uintptr(oldstderr), "oldstderr")}
 		}
 
 		//dupe the output file onto stderr so that output goes there
 		if err = syscall.Dup3(int(fout.Fd()), int(os.Stderr.Fd()), 0); err != nil {
 			fout.Close()
+		}
+	}
+	lgr = NewLevelRelay(clr)
+	return
+}
+
+type critLevelRelay struct {
+	wc []io.WriteCloser
+}
+
+func (c critLevelRelay) WriteLog(l Level, ts time.Time, rfcline, rawline string) (err error) {
+	if l >= ERROR {
+		if _, err = io.WriteString(os.Stderr, rawline); err != nil {
+			return
+		}
+		for _, w := range c.wc {
+			if _, err = io.WriteString(w, rawline); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func (c critLevelRelay) Close() (err error) {
+	for _, wc := range c.wc {
+		if lerr := wc.Close(); err != nil {
+			err = lerr
 		}
 	}
 	return
