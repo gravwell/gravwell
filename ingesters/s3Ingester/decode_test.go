0fcf9053c61d9eb3f1e729f2eeae19723c723b24
Kristopher Watts <108431167+kris-watts-gravwell@users.noreply.github.com>
2023-12-17 10:51:49 -0700

    Update decode_test.go

--- ingesters/s3Ingester/decode_test.go
+++ ingesters/s3Ingester/decode_test.go
@@ -10,17 +10,17 @@ var snsWithBucket = []byte(`{
   "Timestamp" : "2023-12-16T00:21:11.830Z",
   "SignatureVersion" : "1",
   "Signature" : "WnFwMXQWNiwhJtBbff9I5S0UE1Jcnv2V0oeXRPlylS3eB4hO4K50ZXnZSLJePE8zZOXq1y7aIKaIVOs1EPIk7tv8kBwC5r5dAgqc7x4Kf0OI+4DDBwjKBC9LAeUB6b5A8BAiLna+qzzZ0nsRoelVSRDqo+goKfRT+oIP+gX6fQJ84WoLMNDtrZgbI+qevlFcXw7lH/3f34LWpLnLwARXnevEpPeaR45NtYzzvhtIf1qHw6ySdbQ/T8QhaKNu2BHinoqmtNYBMUO7nqyU5Ako/Wgx/R2z2JyjXmzK3DBQNb2+AsDxLCZ5zgBCVZzeHirjtpl64zw5yTRHko2TddbPeg==",
-  "SigningCertURL" : "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-01d088a6f77103d0fe307c0069e40ed6.pem",
-  "UnsubscribeURL" : "https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:005292802539:aws-cloudtrail-logs-005292802539-c48c67bc:b05437d7-1791-4bcc-8ab1-d7c0280a9433"
+  "SigningCertURL" : "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-ABCDABCDABCDABCDABCDABCDABCDABCD.pem",
+  "UnsubscribeURL" : "https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:123456789012:aws-cloudtrail-logs-123456789012-123456789:09c307ae-9d04-11ee-849c-2feff9d344f5"
 }`)
 
 var snsWithRecords = []byte(`
 {
   "Type" : "Notification",
-  "MessageId" : "377fddc7-8d97-5c65-8acd-80316053ccd7",
+  "MessageId" : "25c7149a-9d04-11ee-bbdf-0781f22bc150",
   "TopicArn" : "arn:aws:sns:us-east-1:000:security-cloudtrail",
   "Subject" : "Amazon S3 Notification",
-  "Message" : "{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-east-1\",\"eventTime\":\"2023-12-16T00:12:50.875Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"AWS:foo:regionalDeliverySession\"},\"requestParameters\":{\"sourceIPAddress\":\"1.1.1.1\"},\"responseElements\":{\"x-amz-request-id\":\"foo\",\"x-amz-id-2\":\"foofoo\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"56559d9f-e8a5-461a-ad4e-dff6dfc2734b\",\"bucket\":{\"name\":\"cloudtrail\",\"ownerIdentity\":{\"principalId\":\"foo\"},\"arn\":\"arn:aws:s3:::cloudtrail\"},\"object\":{\"key\":\"foo.json.gz\",\"size\":18394,\"eTag\":\"foo\",\"versionId\":\"x\",\"sequencer\":\"00657CEB82A8FEBAAD\"}}}]}",
+  "Message" : "{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-east-1\",\"eventTime\":\"2023-12-16T00:12:50.875Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"AWS:foo:regionalDeliverySession\"},\"requestParameters\":{\"sourceIPAddress\":\"1.1.1.1\"},\"responseElements\":{\"x-amz-request-id\":\"foo\",\"x-amz-id-2\":\"foofoo\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"64fcd58c-9d04-11ee-a6cc-ebc3a9cee567\",\"bucket\":{\"name\":\"cloudtrail\",\"ownerIdentity\":{\"principalId\":\"foo\"},\"arn\":\"arn:aws:s3:::cloudtrail\"},\"object\":{\"key\":\"foo.json.gz\",\"size\":18394,\"eTag\":\"foo\",\"versionId\":\"x\",\"sequencer\":\"012345678901234567\"}}}]}",
   "Timestamp" : "2023-12-16T00:12:52.342Z",
   "SignatureVersion" : "1",
   "Signature" : "foo",
@@ -30,7 +30,7 @@ var snsWithRecords = []byte(`
 `)
 
 var s3Record = []byte(`
-  {"Records":[{"eventVersion":"2.1","eventSource":"aws:s3","awsRegion":"us-east-1","eventTime":"2023-12-16T00:12:50.875Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:foo:regionalDeliverySession"},"requestParameters":{"sourceIPAddress":"1.1.1.1"},"responseElements":{"x-amz-request-id":"foo","x-amz-id-2":"foofoo"},"s3":{"s3SchemaVersion":"1.0","configurationId":"56559d9f-e8a5-461a-ad4e-dff6dfc2734b","bucket":{"name":"cloudtrail","ownerIdentity":{"principalId":"foo"},"arn":"arn:aws:s3:::cloudtrail"},"object":{"key":"foo.json.gz","size":18394,"eTag":"foo","versionId":"x","sequencer":"00657CEB82A8FEBAAD"}}}]}
+  {"Records":[{"eventVersion":"2.1","eventSource":"aws:s3","awsRegion":"us-east-1","eventTime":"2023-12-16T00:12:50.875Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:foo:regionalDeliverySession"},"requestParameters":{"sourceIPAddress":"1.1.1.1"},"responseElements":{"x-amz-request-id":"foo","x-amz-id-2":"foofoo"},"s3":{"s3SchemaVersion":"1.0","configurationId":"64fcd58c-9d04-11ee-a6cc-ebc3a9cee567","bucket":{"name":"cloudtrail","ownerIdentity":{"principalId":"foo"},"arn":"arn:aws:s3:::cloudtrail"},"object":{"key":"foo.json.gz","size":18394,"eTag":"foo","versionId":"x","sequencer":"012345678901234567"}}}]}
 `)
 
 func TestDecodeSNS(t *testing.T) {
