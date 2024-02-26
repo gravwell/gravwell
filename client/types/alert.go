0642d852af0ee512d4e6472c4601f375c8d5d00a
John Floren <john@jfloren.net>
2024-02-26 14:31:12 -0800

    Add WriteAccess and Can fields to AlertDefinition type.

--- client/types/alert.go
+++ client/types/alert.go
@@ -33,6 +33,7 @@ const (
 
 // AlertDefinition - A Gravwell Alert specification
 type AlertDefinition struct {
+	Can Actions `json:"Can"`
 
 	// A list of flows which will be run when alerts are generated.
 	Consumers []AlertConsumer `json:"Consumers"`
@@ -80,6 +81,8 @@ type AlertDefinition struct {
 
 	// Arbitrary user-defined metadata which will be injected into the events
 	UserMetadata map[string]interface{} `json:"UserMetadata"`
+
+	WriteAccess Access `json:"WriteAccess"`
 }
 
 // AlertConsumer - Something which consumes alerts.
