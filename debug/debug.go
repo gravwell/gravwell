792337a5d8408aebe29a235e924c4d6cd43b3a47
David Fritz <fritz@gravwell.io>
2024-07-19 13:03:08 -0600

    address comments

--- debug/debug.go
+++ debug/debug.go
@@ -11,71 +11,85 @@ import (
 	"time"
 )
 
-const CPU_SLEEP = 10 * time.Second
+const (
+	CPU_SLEEP      = 10 * time.Second
+	MAX_STACK_SIZE = 256 * 1024 * 1024
+)
 
 // HandleDebugSignals is a SIGUSR1 trap that can be installed at the beginning
 // of runtime to generate a stack trace, memory profile, and CPU profile. It
 // takes a name to be used as a directory prefix, and creates files in the
 // system temporary directory.
 func HandleDebugSignals(name string) {
-	for {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGUSR1)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGUSR1)
 
-		<-c
-
+	for range c {
 		// get files prepped
 		dir, err := os.MkdirTemp("", name)
 		if err != nil {
 			continue
 		}
-		stackTraceName := filepath.Join(dir, "stack")
-		memName := filepath.Join(dir, "mem.prof")
-		cpuName := filepath.Join(dir, "cpu.prof")
 
-		st, err := os.Create(stackTraceName)
-		if err != nil {
-			continue
-		}
-		mem, err := os.Create(memName)
-		if err != nil {
-			continue
-		}
-		cpu, err := os.Create(cpuName)
-		if err != nil {
-			continue
-		}
+		generateStackTrace(dir)
+		generateMemoryProfile(dir)
+		generateCPUProfile(dir)
+	}
+}
 
-		// return a trace, growing the buffer until it's big enough
-		size := 1024 * 1024
-		var buf []byte
-		var n int
-		for {
-			buf = make([]byte, size)
-			n = runtime.Stack(buf, true)
-			if n < size {
-				break
-			}
-			size *= 2
-		}
-		st.Write(buf[:n])
-		st.Close()
+func generateStackTrace(dir string) {
+	stackTraceName := filepath.Join(dir, "stack")
+	st, err := os.Create(stackTraceName)
+	if err != nil {
+		return
+	}
 
-		// return a memory profile
-		membuf := &bytes.Buffer{}
-		runtime.GC()
-		if err := pprof.WriteHeapProfile(membuf); err == nil {
-			mem.Write(membuf.Bytes())
-			mem.Close()
+	// return a trace, growing the buffer until it's big enough
+	size := 1024 * 1024
+	var buf []byte
+	var n int
+	for {
+		buf = make([]byte, size)
+		n = runtime.Stack(buf, true)
+		if n < size {
+			break
 		}
+		size *= 2
+		if size >= MAX_STACK_SIZE {
+			return
+		}
+	}
+	st.Write(buf[:n])
+	st.Close()
+}
 
-		// return a cpu profile
-		cpubuf := &bytes.Buffer{}
-		if err := pprof.StartCPUProfile(cpubuf); err == nil {
-			time.Sleep(CPU_SLEEP)
-			pprof.StopCPUProfile()
-			cpu.Write(cpubuf.Bytes())
-			cpu.Close()
-		}
+func generateMemoryProfile(dir string) {
+	memName := filepath.Join(dir, "mem.prof")
+	mem, err := os.Create(memName)
+	if err != nil {
+		return
+	}
+
+	membuf := &bytes.Buffer{}
+	runtime.GC()
+	if err := pprof.WriteHeapProfile(membuf); err == nil {
+		mem.Write(membuf.Bytes())
+		mem.Close()
+	}
+}
+
+func generateCPUProfile(dir string) {
+	cpuName := filepath.Join(dir, "cpu.prof")
+	cpu, err := os.Create(cpuName)
+	if err != nil {
+		return
+	}
+
+	cpubuf := &bytes.Buffer{}
+	if err := pprof.StartCPUProfile(cpubuf); err == nil {
+		time.Sleep(CPU_SLEEP)
+		pprof.StopCPUProfile()
+		cpu.Write(cpubuf.Bytes())
+		cpu.Close()
 	}
 }
