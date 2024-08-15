1bf0f002f2c34c7c17cf7d7cd74ca4c34ad2ac72
kris-watts-gravwell <kris.watts@gravwell.io>
2024-08-15 15:26:50 -0600

    bumping version number for next major

--- ingesters/version/version.go
+++ ingesters/version/version.go
@@ -15,13 +15,13 @@ import (
 )
 
 const (
-	MajorVersion = 5
-	MinorVersion = 6
+	MajorVersion = 6
+	MinorVersion = 0
 	PointVersion = 0
 )
 
 var (
-	BuildDate time.Time = time.Date(2024, 8, 21, 0, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2024, 10, 1, 0, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
