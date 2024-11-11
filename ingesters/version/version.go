74e3fd5e43d11556911470674b5b3ec6dc1b9b66
kris <kris.watts@gravwell.io>
2024-11-11 07:54:19 -0700

    bumping version

--- ingesters/version/version.go
+++ ingesters/version/version.go
@@ -17,11 +17,11 @@ import (
 const (
 	MajorVersion = 5
 	MinorVersion = 6
-	PointVersion = 5
+	PointVersion = 6
 )
 
 var (
-	BuildDate time.Time = time.Date(2024, 11, 8, 0, 0, 0, 0, time.UTC)
+	BuildDate time.Time = time.Date(2024, 11, 11, 0, 0, 0, 0, time.UTC)
 )
 
 func PrintVersion(wtr io.Writer) {
