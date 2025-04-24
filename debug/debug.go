0dae61514a002825272c9040778ea4fc0da68291
kris <kris.watts@gravwell.io>
2025-04-24 11:34:28 -0600

    more staticcheck

--- debug/debug.go
+++ debug/debug.go
@@ -1,3 +1,12 @@
+/*************************************************************************
+ * Copyright 2025 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+// Package debug implements some debug helpers that are used in all ingesters
 package debug
 
 import (
