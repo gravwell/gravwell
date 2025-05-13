4d82d4c362b0ec1873395fb9497143526ea0847e
R Landau <rory.landau@gravwell.io>
2025-05-13 12:19:18 -0700

    implement runTest helper

--- gwcli/magefiles/magefile.go
+++ gwcli/magefiles/magefile.go
@@ -39,6 +39,20 @@ func ok() {
 	verboseln(green + "ok" + reset)
 }
 
+// Runs the given test and outputs (verbose-dependent) its error log (or "ok").
+// Returns the error that occurred (if applicable).
+func runTest(timeout time.Duration, testPattern, testPath string) error {
+	cmd := exec.Command("go", "test", "-v", "-timeout", timeout.String(), "-run", testPattern, testPath)
+	verboseln(cmd.String())
+	if out, err := cmd.CombinedOutput(); err != nil {
+		fmt.Printf("%s", out)
+		return err
+	}
+	ok()
+
+	return nil
+}
+
 //#endregion
 
 //#region setup
@@ -66,13 +80,16 @@ func Build() error {
 	}
 
 	output := path.Join(pwd, _BINARY_TARGET)
-	verboseln("Building...")
+	verboseln("Building " + output + "...")
 	cmd := exec.Command("go", "build", "-o", output, ".")
 	return cmd.Run()
 }
 
 // Runs all gwcli tests.
 func TestAll() error {
+	verboseln("Testing query components...")
+	mg.Deps(TestQuery, TestDatascope)
+
 	mg.Deps(TestMain)
 	return nil
 }
@@ -80,20 +97,35 @@ func TestAll() error {
 // Runs the top-level tests that mimic scripted user input.
 func TestMain() error {
 	const _TIMEOUT time.Duration = 25 * time.Second
-	cmd := exec.Command("go", "test", "-v", "-timeout", _TIMEOUT.String(), "-run", "^TestNonInteractive$", "github.com/gravwell/gravwell/v4/gwcli")
-	verboseln(cmd.String())
-	if out, err := cmd.CombinedOutput(); err != nil {
-		fmt.Printf("failed! %v\n%s", err, out)
+	if err := runTest(_TIMEOUT, "^TestNonInteractive$", "github.com/gravwell/gravwell/v4/gwcli"); err != nil {
 		return err
 	}
-	ok()
+	if err := runTest(_TIMEOUT, "^TestNonInteractiveQueryFileOut$", "github.com/gravwell/gravwell/v4/gwcli"); err != nil {
+		return err
+	}
+	return nil
+}
 
-	cmd = exec.Command("go", "test", "-timeout", _TIMEOUT.String(), "-run", "^TestNonInteractiveQueryFileOut$", "github.com/gravwell/gravwell/v4/gwcli")
-	verboseln(cmd.String())
-	if err := cmd.Run(); err != nil {
+// Tests the Query system.
+func TestQuery() error {
+	const _TIMEOUT time.Duration = 20 * time.Second
+	if err := runTest(_TIMEOUT, "^Test_tryQuery$", "github.com/gravwell/gravwell/v4/gwcli/tree/query"); err != nil {
 		return err
 	}
-	ok()
+
+	if err := runTest(_TIMEOUT, "^Test_run$", "github.com/gravwell/gravwell/v4/gwcli/tree/query"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Tests the Datascope query subsystem.
+func TestDatascope() error {
+	const _TIMEOUT time.Duration = 4 * time.Minute
+	if err := runTest(_TIMEOUT, "^TestKeepAlive$", "github.com/gravwell/gravwell/v4/gwcli/tree/query/datascope"); err != nil {
+		return err
+	}
 
 	return nil
 }
