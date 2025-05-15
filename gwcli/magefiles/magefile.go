9bdf05cbcedff5a3ab82a6500daab543ab552fe5
R Landau <rory.landau@gravwell.io>
2025-05-15 07:17:34 -0700

    include copyright notice

--- gwcli/magefiles/magefile.go
+++ gwcli/magefiles/magefile.go
@@ -1,3 +1,11 @@
+/*************************************************************************
+ * Copyright 2025 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
 /*
 The build system for gwcli, built on Mage.
 Because it is self-contained, you can also just use go build inside of the gwcli directory
