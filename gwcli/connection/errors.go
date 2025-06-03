0e91a8f1e98a8531a85b161d8bb96d262a7df1b9
R Landau <rory.landau@gravwell.io>
2025-06-03 14:54:25 -0700

    move error

--- gwcli/connection/errors.go
+++ gwcli/connection/errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 var ErrAPITokenRequired error = errors.New("MFA is enabled, API token is required")
 var ErrCredentialsOrAPITokenRequired error = errors.New("Credentials or API token required")
+var ErrNotInitialized error = errors.New("client must be initialized")
