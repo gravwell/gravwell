9e9132c41690fac4e3512711ff8c3c2db384478d
R Landau <rory.landau@gravwell.io>
2025-06-03 16:49:52 -0700

    add errors

--- gwcli/connection/errors.go
+++ gwcli/connection/errors.go
@@ -2,6 +2,10 @@ package connection
 
 import "errors"
 
-var ErrAPITokenRequired error = errors.New("MFA is enabled, API token is required")
-var ErrCredentialsOrAPITokenRequired error = errors.New("Credentials or API token required")
-var ErrNotInitialized error = errors.New("client must be initialized")
+var (
+	ErrAPITokenRequired              error = errors.New("MFA is enabled, API token is required")
+	ErrCredentialsOrAPITokenRequired error = errors.New("Credentials or API token required")
+	ErrNotInitialized                error = errors.New("client must be initialized")
+	ErrAPIKeyInvalid                 error = errors.New("API key could not be validated")
+	ErrMFASetupRequired              error = errors.New("MFA is required. Please log in via the browser to set it up.")
+)
