25ae100f09b553a1a0596448c25aec00079cbbcf
R Landau <rory.landau@gravwell.io>
2025-06-12 11:53:05 -0700

    implement interactive usage

--- gwcli/tree/ingest/actor.go
+++ gwcli/tree/ingest/actor.go
@@ -1,76 +1,160 @@
+/*************************************************************************
+ * Copyright 2025 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
 package ingest
 
 import (
 	"fmt"
+	"net/netip"
 	"os"
 
 	"github.com/charmbracelet/bubbles/filepicker"
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/gravwell/gravwell/v4/gwcli/action"
+	"github.com/gravwell/gravwell/v4/gwcli/busywait"
 	"github.com/gravwell/gravwell/v4/gwcli/clilog"
+	"github.com/gravwell/gravwell/v4/gwcli/connection"
 	"github.com/gravwell/gravwell/v4/gwcli/stylesheet"
 	"github.com/gravwell/gravwell/v4/gwcli/stylesheet/colorizer"
 	"github.com/gravwell/gravwell/v4/gwcli/utilities/uniques"
 	"github.com/spf13/pflag"
 )
 
+type mode = string
+
+const (
+	picking   mode = "picking"
+	ingesting mode = "ingesting"
+	done      mode = "done"
+)
+
 var Ingest action.Model = Initial()
 
 type ingest struct {
-	fp           filepicker.Model
-	selectedFile string
-	quitting     bool
-	err          error
+	fp          filepicker.Model
+	mode        mode
+	err         error
+	ingestResCh chan error
 
 	// modifier pane items
 	modFocused bool            // is the modifier pane in focus?
+	tagTI      textinput.Model // tag to ingest file under
 	srcTI      textinput.Model // user-provided IP address source
 	ignoreTS   bool
 	localTime  bool
+
+	spinner spinner.Model // TODO should busywait be pushed into stylesheet?
 }
 
 func Initial() *ingest {
 	i := &ingest{
-		fp:    filepicker.New(),
+		fp:          filepicker.New(),
+		mode:        picking,
+		ingestResCh: make(chan error),
+
 		srcTI: stylesheet.NewTI("", true),
+		tagTI: stylesheet.NewTI("default", true),
 	}
 
 	i.srcTI.Placeholder = "127.0.0.1"
-
-	// TODO
 
 	return i
 }
 
 func (i *ingest) Update(msg tea.Msg) tea.Cmd {
-	if keyMsg, ok := msg.(tea.KeyMsg); ok {
-		switch keyMsg.Type {
-		case tea.KeyTab:
-			// switch focus
-			i.modFocused = !i.modFocused
+	switch i.mode {
+	case done: // wait for mother to take over
+		return nil
+	case ingesting: // wait for results
+		// check if we are done
+		select {
+		case err := <-i.ingestResCh:
+			i.mode = done
+			// print the error above the TUI; the other goro logs for us
+			if err != nil {
+				return tea.Println(stylesheet.ErrStyle.Render(err.Error()))
+			}
 			return nil
-		case tea.KeyEnter:
-			// check that src is empty or a valid IP
-			// TODO
-			// attempt ingestion
-			// TODO
+		default:
+			return i.spinner.Tick
+		}
+	default: //case picking:
+		if keyMsg, ok := msg.(tea.KeyMsg); ok {
+			i.err = nil
+			switch keyMsg.Type {
+			case tea.KeyTab:
+				// switch focus
+				i.modFocused = !i.modFocused
+				return nil
+			case tea.KeyEnter:
+				// check that src is empty or a valid IP
+				src := i.srcTI.Value()
+				if src != "" {
+					if _, err := netip.ParseAddr(src); err != nil {
+						// set error and return
+						i.err = err
+						return nil
+					}
+				}
+
+				// check that a file has been selected
+				file := i.fp.FileSelected
+				tag := i.tagTI.Value()
+
+				i.mode = ingesting
+
+				// spin ingestion off into goroutine
+				clilog.Writer.Warnf("ingesting file %v with parameters: tag='%v' src='%v' ignore=%v local=%v",
+					file, tag, src, i.ignoreTS, i.localTime)
+				go func() {
+					resp, err := connection.Client.IngestFile(file, tag, src, i.ignoreTS, i.localTime)
+					if err != nil {
+						clilog.Writer.Warnf("failed to ingest file %v: %v", file, err)
+						// spit above TUI and boot the user back out
+					}
+					clilog.Writer.Infof("successfully ingested file %v: %+v", file, resp)
+				}()
+
+				// start a spinner and wait
+				i.spinner = busywait.NewSpinner()
+			}
+
 		}
 
+		return nil
 	}
-
-	return nil
 }
 
 func (i *ingest) View() string {
 	// build modifier view
 	modView := fmt.Sprintf("Ignore Timestamps? %v\t"+
 		"Use Server Local Time? %v\t"+
-		"source: %v",
+		"source: %s\t"+
+		"tag: %s",
 		colorizer.Checkbox(i.ignoreTS),
 		colorizer.Checkbox(i.localTime),
-		i.srcTI.View())
+		i.srcTI.View(),
+		i.tagTI.View())
+
+	// TODO add spinner and second TI
+
+	var spnrErrHelp string
+	if i.mode == ingesting {
+		spnrErrHelp = i.spinner.View()
+	} else if i.err != nil {
+		spnrErrHelp = stylesheet.ErrStyle.Render(i.err.Error())
+	} else {
+		// TODO
+		spnrErrHelp = "" // display help keys for submission and changing focus
+	}
 
 	// wrap it in a border
 	if i.modFocused {
@@ -80,18 +164,20 @@ func (i *ingest) View() string {
 	}
 
 	// compose views
-	return lipgloss.JoinVertical(lipgloss.Center, i.fp.View(), modView)
+	return lipgloss.JoinVertical(lipgloss.Center, i.fp.View(), modView, spnrErrHelp)
 }
 
-func (i *ingest) Done() bool { return true }
+func (i *ingest) Done() bool {
+	return i.mode == done
+}
 
 func (i *ingest) Reset() error {
-	//i.fp
-	i.selectedFile = ""
-	i.quitting = false
+	//i.fp // TODO does this need to be reset?
+	i.mode = picking
 	i.err = nil
 
 	i.modFocused = false
+	i.tagTI.Reset()
 	i.srcTI.Reset()
 	i.ignoreTS = false
 	i.localTime = false
@@ -131,6 +217,11 @@ func (i *ingest) SetArgs(_ *pflag.FlagSet, tokens []string) (string, tea.Cmd, er
 		clilog.Writer.Warnf("failed to get pwd: %v", err)
 		i.fp.CurrentDirectory = "." // allow OS to decide where to drop us
 	}
+
+	// if one+ files were given, try to ingest immediately
+	// TODO
+
+	// update the spinning view to just declare the file(s) that are being ingested
 
 	return "", i.fp.Init(), nil
 }
