96d9e891ac968a71e9129d3401ffdc4604338234
R Landau <rory.landau@gravwell.io>
2025-06-12 13:49:34 -0700

    rework to accept a single channel to write responses to

--- gwcli/tree/ingest/shared.go
+++ gwcli/tree/ingest/shared.go
@@ -6,29 +6,26 @@ import (
 	"github.com/gravwell/gravwell/v4/gwcli/connection"
 )
 
-// autoingest attempts to ingest the data at each file path, returning errors and successes on their respective channels.
+// autoingest attempts to ingest the data at each file path, returning errors and successes on the given channel.
 // Performs ingestions in parallel; once len(filepaths) results have been send (cumulative across both channels), caller can assume this goroutine has returned.
 // No logging is performed internally; caller is expected to log and present results.
 //
-// If ufErr (user-friendly error) is returned, do not wait on the channels; they will be nil.
-func autoingest(filepaths, tags []string, ignoreTS, localTime bool, src string) (
-	ingestSuccess <-chan string,
-	ingestErr <-chan struct {
-		string
-		error
-	}, ufErr error) {
+// If ufErr (user-friendly error) is returned, do not wait on the channel; no values will be sent.
+func autoingest(res chan<- struct {
+	string
+	error
+}, filepaths, tags []string, ignoreTS, localTime bool, src string) (ufErr error) {
 	// check that tag len is 1 or == file len
 	if len(tags) != 1 && len(tags) != len(filepaths) {
-		return nil, nil, fmt.Errorf("tag count must be 1 or equal to the number of files specified (%v)", len(filepaths))
+		return fmt.Errorf("tag count must be 1 or equal to the number of files specified (%v)", len(filepaths))
 	}
-
-	successCh, errCh := make(chan string), make(chan struct {
-		string
-		error
-	})
 
 	// try to ingest each file
 	for i, fp := range filepaths {
+		if fp == "" {
+			continue
+		}
+
 		go func() {
 			var tag string
 			if len(tags) == 1 {
@@ -38,15 +35,12 @@ func autoingest(filepaths, tags []string, ignoreTS, localTime bool, src string) 
 			}
 
 			_, err := connection.Client.IngestFile(fp, tag, src, ignoreTS, localTime)
-			if err != nil {
-				errCh <- struct {
-					string
-					error
-				}{fp, err}
-				return
-			}
-			successCh <- fp
+			res <- struct {
+				string
+				error
+			}{fp, err}
+			return
 		}()
 	}
-	return successCh, errCh, nil
+	return nil
 }
