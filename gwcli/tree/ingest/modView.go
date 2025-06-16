e197195a04271f435e448eb55b649173237355cb
R Landau <rory.landau@gravwell.io>
2025-06-16 12:39:07 -0700

    correct interactivity

--- gwcli/tree/ingest/modView.go
+++ gwcli/tree/ingest/modView.go
@@ -9,20 +9,24 @@ import (
 	"github.com/gravwell/gravwell/v4/gwcli/stylesheet/colorizer"
 )
 
+// currently selected item in the mod view for pips and focus
 type modItem = uint
 
 const (
 	lowBound modItem = iota
-	tag
 	src
+	tag
 	ignoreTS
 	localTime
 	highBound
 )
 
+// mod struct represents the state of the modifier/excess details pane
 type mod struct {
+	// meta
 	focused  bool    // is the modifier pane in focus?
 	selected modItem // currently selected item in the mod pane
+	width    uint
 
 	tagTI     textinput.Model // tag to ingest file under
 	srcTI     textinput.Model // user-provided IP address source
@@ -33,12 +37,14 @@ type mod struct {
 func NewMod() mod {
 	m := mod{
 		focused:  false,
-		selected: tag,
+		selected: src,
 
 		tagTI: stylesheet.NewTI("", true),
 		srcTI: stylesheet.NewTI("default", true),
 	}
 	m.srcTI.Placeholder = "127.0.0.1"
+	m.srcTI.Focus()
+	m.tagTI.Blur()
 	return m
 }
 
@@ -46,18 +52,26 @@ func NewMod() mod {
 func (m mod) update(msg tea.Msg) (mod, tea.Cmd) {
 	if keyMsg, ok := msg.(tea.KeyMsg); ok {
 		switch keyMsg.Type {
-		case tea.KeyUp:
+		case tea.KeyLeft:
 			m.selected -= 1
 			if m.selected <= lowBound {
 				m.selected = highBound - 1
 			}
 			m.focusSelected()
-		case tea.KeyDown:
+		case tea.KeyRight:
 			m.selected += 1
 			if m.selected >= highBound {
 				m.selected = lowBound + 1
 			}
 			m.focusSelected()
+		case tea.KeySpace:
+			// toggle the selected boolean
+			switch m.selected {
+			case ignoreTS:
+				m.ignoreTS = !m.ignoreTS
+			case localTime:
+				m.localTime = !m.localTime
+			}
 		}
 	}
 	var cmds = []tea.Cmd{nil, nil}
@@ -68,19 +82,21 @@ func (m mod) update(msg tea.Msg) (mod, tea.Cmd) {
 }
 
 func (m mod) view() string {
-	v := fmt.Sprintf("%vIgnore Timestamps? %v\t"+
-		"%vUse Server Local Time? %v\t"+
+	v := fmt.Sprintf(
 		"%vsource: %s\t"+
-		"%vtag: %s",
+			"%vtag: %s\t"+
+			"%vIgnore Timestamps? %v\t"+
+			"%vUse Server Local Time? %v\t",
+		colorizer.Pip(m.selected, src), m.srcTI.View(),
+		colorizer.Pip(m.selected, tag), m.tagTI.View(),
 		colorizer.Pip(m.selected, ignoreTS), colorizer.Checkbox(m.ignoreTS),
 		colorizer.Pip(m.selected, localTime), colorizer.Checkbox(m.localTime),
-		colorizer.Pip(m.selected, src), m.srcTI.View(),
-		colorizer.Pip(m.selected, tag), m.tagTI.View())
+	)
 
 	if m.focused {
-		return stylesheet.Sheet.Composable.FocusedBorder.Render(v)
+		return stylesheet.Sheet.Composable.FocusedBorder.Width(int(m.width) - (stylesheet.Sheet.Composable.FocusedBorder.GetHorizontalMargins() + 2)).Render(v)
 	} else {
-		return stylesheet.Sheet.Composable.UnfocusedBorder.Render(v)
+		return stylesheet.Sheet.Composable.UnfocusedBorder.Width(int(m.width) - (stylesheet.Sheet.Composable.UnfocusedBorder.GetHorizontalMargins() + 2)).Render(v)
 	}
 }
 
@@ -91,6 +107,9 @@ func (m mod) reset() mod {
 	m.srcTI.Reset()
 	m.ignoreTS = false
 	m.localTime = false
+
+	m.srcTI.Focus()
+	m.tagTI.Blur()
 
 	return m
 }
