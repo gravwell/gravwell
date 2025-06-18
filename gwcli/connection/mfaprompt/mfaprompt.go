0256f37987d2a6dbd0cbc611153928aaa0fde911
R Landau <rory.landau@gravwell.io>
2025-06-18 10:24:39 -0700

    do not double up on TI symbol

--- gwcli/connection/mfaprompt/mfaprompt.go
+++ gwcli/connection/mfaprompt/mfaprompt.go
@@ -81,7 +81,7 @@ type mfaModel struct {
 func New() mfaModel {
 	c := mfaModel{codeSelected: true}
 	c.codeTI = textinput.New()
-	c.codeTI.Prompt = stylesheet.Cur.PromptSty.Symbol()
+	c.codeTI.Prompt = ""
 	c.codeTI.Validate = func(s string) error {
 		for _, r := range s {
 			if !unicode.IsDigit(r) {
@@ -96,7 +96,7 @@ func New() mfaModel {
 	c.codeTI.Focus()
 
 	c.recoveryTI = textinput.New()
-	c.recoveryTI.Prompt = stylesheet.Cur.PromptSty.Symbol()
+	c.recoveryTI.Prompt = ""
 	c.recoveryTI.Blur()
 
 	return c
