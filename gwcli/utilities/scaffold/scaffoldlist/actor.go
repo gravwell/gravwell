281c061bce63b19d9a7bc45c4afbf90cdbcb90b7
R Landau <rory.landau@gravwell.io>
2025-07-01 12:53:18 -0700

    split actor into its own file

---
+++ gwcli/utilities/scaffold/scaffoldlist/actor.go
@@ -0,0 +1,188 @@
+/*************************************************************************
+ * Copyright 2025 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+package scaffoldlist
+
+// This file defines interactive usage of the scaffolded action.
+// The defined action satisfies the action.Model interface.
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/gravwell/gravwell/v4/gwcli/action"
+	"github.com/gravwell/gravwell/v4/gwcli/clilog"
+	"github.com/gravwell/gravwell/v4/utils/weave"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+type ListAction[dataStruct any] struct {
+	// data cleared by .Reset()
+	done        bool
+	columns     []string
+	showColumns bool          // print columns and exit
+	fs          pflag.FlagSet // current flagset, parsed or unparsed
+	outFile     *os.File      // file to output results to (or nil)
+
+	// data shielded from .Reset()
+	DefaultFormat  outputFormat
+	DefaultColumns []string // columns to output if unspecified
+	//afsFunc        AddtlFlagFunction // the additional flagset to add to the starter when restoring
+	color bool           // inferred from the global "--no-color" flag
+	cmd   *cobra.Command // the command associated to this list action
+
+	// individualized for each use of scaffoldlist
+	dataStruct       dataStruct
+	dataFunc         ListDataFunction[dataStruct] // function for fetching data for table/json/csv
+	prettyFunc       PrettyPrinterFunc
+	addtlFlagSetFunc AddtlFlagFunction // function to regenerate the additional flags, as all FlagSet copies are shallow
+}
+
+// Constructs a ListAction suitable for interactive use.
+// Options are execution in array-order.
+func newListAction[Any any](defaultColumns []string, dataStruct Any, dFn ListDataFunction[Any], options Options) ListAction[Any] {
+	la := ListAction[Any]{
+		done:    false,
+		columns: defaultColumns,
+		fs:      *buildFlagSet(options.AddtlFlags, options.Pretty != nil),
+
+		DefaultFormat:  tbl,
+		DefaultColumns: defaultColumns,
+		color:          true,
+		cmd:            nil,
+
+		dataStruct:       dataStruct,
+		dataFunc:         dFn,
+		prettyFunc:       options.Pretty,
+		addtlFlagSetFunc: options.AddtlFlags}
+
+	return la
+}
+
+// Update takes in a msg (some event that occurred, like a window redraw or a key press) and acts on it.
+// List only ever needs to update once; it figured out what data is to be displayed, fetches it, and spits it out above the prompt.
+func (la *ListAction[T]) Update(msg tea.Msg) tea.Cmd {
+	if la.done {
+		return nil
+	}
+
+	// list only ever acts once; immediately mark it as done
+	la.done = true
+
+	// check for --show-columns
+	if la.showColumns {
+		cols, err := weave.StructFields(la.dataStruct, true)
+		if err != nil {
+			tea.Println("An error has occurred: ", err)
+			return textinput.Blink
+		}
+		return tea.Println(strings.Join(cols, " "))
+	}
+
+	// fetch the list data
+	s, err := listOutput(&la.fs, la.columns, la.color, la.dataFunc)
+	if err != nil {
+		// log and print the error
+		clilog.Writer.Error(err.Error())
+		return tea.Println("An error has occurred: ", err)
+	}
+
+	// if we received no data, note that (unless we are printing to a file, then do nothing)
+	if s == "" {
+		if la.outFile != nil {
+			return textinput.Blink
+		}
+		return tea.Println("no data found")
+	}
+
+	// output the results to a file, if given
+	if la.outFile != nil {
+		fmt.Fprint(la.outFile, s)
+		return tea.Println("Successfully output results to " + la.outFile.Name())
+	}
+
+	return tea.Println(s)
+}
+
+// View is called after each update cycle to redraw dynamic content,
+// but is not used by list actions as they output all of their data rather than dynamically viewing it.
+func (la *ListAction[T]) View() string {
+	return ""
+}
+
+// Done is called once per cycle to test if Mother should reassert control
+func (la *ListAction[T]) Done() bool {
+	return la.done
+}
+
+// Reset is called when the action is unseated by Mother on exiting handoff mode
+func (la *ListAction[T]) Reset() error {
+	la.done = false
+	la.columns = la.DefaultColumns
+	la.showColumns = false
+
+	la.fs = listStarterFlags()
+	// if we were given additional flags, add them
+	if la.addtlFlagSetFunc != nil {
+		afs := la.addtlFlagSetFunc()
+		la.fs.AddFlagSet(&afs)
+	}
+
+	if la.outFile != nil {
+		la.outFile.Close()
+	}
+	la.outFile = nil
+	return nil
+}
+
+var _ action.Model = &ListAction[any]{}
+
+// SetArgs is called when the action is invoked by the user and Mother *enters* handoff mode.
+// Mother parses flags and provides us a handle to check against.
+func (la *ListAction[T]) SetArgs(
+	inherited *pflag.FlagSet, tokens []string) (invalid string, onStart tea.Cmd, err error) {
+
+	// attach inherited flags to the normal flagset
+	la.fs.AddFlagSet(inherited)
+
+	err = la.fs.Parse(tokens)
+	if err != nil {
+		return err.Error(), nil, nil
+	}
+	//fs := la.fs
+
+	// parse column handling
+	// only need to parse columns if user did not pass in --show-columns
+	if la.showColumns, err = la.fs.GetBool("show-columns"); err != nil {
+		return "", nil, err
+	} else if !la.showColumns {
+		// fetch columns if it exists
+		if cols, err := la.fs.GetStringSlice("columns"); err != nil {
+			return "", nil, err
+		} else if len(cols) > 0 {
+			la.columns = cols
+		} // else: defaults to DefaultColumns
+	}
+
+	nc, err := la.fs.GetBool("no-color")
+	if err != nil {
+		la.color = false
+		clilog.Writer.Warnf("Failed to fetch no-color from inherited: %v", err)
+	}
+	la.color = !nc
+
+	if f, err := initOutFile(&la.fs); err != nil {
+		return "", nil, err
+	} else {
+		la.outFile = f
+	}
+
+	return "", nil, nil
+}
