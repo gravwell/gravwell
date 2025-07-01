14500bdec4882803cc72a17a137d99f1f639d43f
R Landau <rory.landau@gravwell.io>
2025-07-01 10:17:50 -0700

    create option struct

---
+++ gwcli/utilities/scaffold/scaffoldlist/options.go
@@ -0,0 +1,28 @@
+/*************************************************************************
+ * Copyright 2025 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
+package scaffoldlist
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+// The Options struct allows developers to tweak parameters of an action's specific implementation.
+type Options struct {
+	// Overrides the default "list" action name.
+	Use string
+	// Pretty defines a free-form, pretty-printing function, allowing this action to be displayed in a user-friendly (albeit likely script-unfriendly) way.
+	// If !nil, --pretty will also be defined and set as the default.
+	Pretty func(*cobra.Command) (string, error)
+	// Replace the default list example.
+	Example string
+	// AddtlFlags defines a function that generates a fresh flagset to be bolted on to the default list flagset.
+	// NOTE(rlandau): It must be a function returning a fresh struct because FlagSets are shallow copies, even when passed by reference.
+	AddtlFlags func() pflag.FlagSet
+}
