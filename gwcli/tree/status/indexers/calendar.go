6ba8b721559d79927396ac7404287bc2f9cbfb31
R Landau <rory.landau@gravwell.io>
2025-07-02 10:40:44 -0700

    default times to now; do not require --start and --end

--- gwcli/tree/status/indexers/calendar.go
+++ gwcli/tree/status/indexers/calendar.go
@@ -11,13 +11,13 @@ package indexers
 // This file implements the indexer calendar action, which returns timestamped entries associated to the named indexer.
 
 import (
-	"errors"
 	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gravwell/gravwell/v4/client/types"
 	"github.com/gravwell/gravwell/v4/gwcli/action"
+	"github.com/gravwell/gravwell/v4/gwcli/clilog"
 	"github.com/gravwell/gravwell/v4/gwcli/connection"
 	"github.com/gravwell/gravwell/v4/gwcli/utilities/scaffold/scaffoldlist"
 	"github.com/gravwell/gravwell/v4/gwcli/utilities/uniques"
@@ -41,9 +41,6 @@ func newCalendarAction() action.Pair {
 			Aliases:    aliases,
 			AddtlFlags: calendarFlags,
 			CmdMods: func(c *cobra.Command) {
-				c.MarkFlagRequired("start")
-				c.MarkFlagRequired("end")
-				//c.MarkFlagsRequiredTogether("start", "end")
 				c.Args = cobra.MaximumNArgs(1) // indexer uuid may be specified
 			},
 		})
@@ -51,13 +48,14 @@ func newCalendarAction() action.Pair {
 
 func calendarFlags() pflag.FlagSet {
 	fs := pflag.FlagSet{}
-	fs.String("start", "", "REQUIRED. Start time for calendar stats.\n"+
+	fs.String("start", "", "start time for calendar stats.\n"+
 		"May be given in RFC1123Z (Mon, 02 Jan 2006 15:04:05 -0700) or DateTime (2006-01-02 15:04:05).\n"+
-		"If --start is given, --end must also be specified.")
-	fs.String("end", "", "REQUIRED. End time for calendar stats.\n"+
+		"If unset, defaults to now.")
+	fs.String("end", "", "end time for calendar stats.\n"+
 		"May be given in RFC1123Z (Mon, 02 Jan 2006 15:04:05 -0700) or DateTime (2006-01-02 15:04:05).\n"+
-		"If --end is given, --start must also be specified.")
-	fs.StringSlice("wells", nil, "specify the wells to fetch data for.")
+		"If unset, defaults to now.")
+	fs.StringSlice("wells", nil, "specify the wells to fetch data for.\n"+
+		"If unspecified, all wells will be selected.")
 	return fs
 }
 
@@ -74,16 +72,24 @@ func data(fs *pflag.FlagSet) ([]types.CalendarEntry, error) {
 	if err != nil {
 		return nil, uniques.ErrGetFlag(useCalendar, err)
 	}
-	// if an indexer was specified, get stats for that specific indexer
-	if fs.NArg() == 1 {
-		indexer, err := uuid.Parse(fs.Arg(0))
+	var indexer = uuid.Nil
+	if fs.NArg() > 0 {
+		idxr, err := uuid.Parse(fs.Arg(0))
 		if err != nil {
 			return nil, err
 		}
-		return connection.Client.GetIndexerCalendarStats(indexer, start, end, wells)
+		indexer = idxr
 	}
 
-	return connection.Client.GetCalendarStats(start, end, wells)
+	clilog.Writer.Infof("fetching calendar entries for indexer %v", indexer)
+	clilog.Writer.Debugf("start=%v|end=%v|wells=%v", start, end, wells)
+
+	// if an indexer was specified, get stats for that specific indexer
+	if indexer != uuid.Nil {
+		return connection.Client.GetIndexerCalendarStats(indexer, start, end, nil)
+	}
+
+	return connection.Client.GetCalendarStats(time.Time{}, end, nil)
 }
 
 // fetchTime attempts to parse the given flag as a time.Time.
@@ -106,5 +112,5 @@ func fetchTime(fs *pflag.FlagSet, flagName string) (time.Time, error) {
 		return t, nil
 	}
 
-	return time.Time{}, errors.New("--" + flagName + " must be a valid timestamp in RFC1123Z or DateTime format")
+	return time.Now(), nil
 }
