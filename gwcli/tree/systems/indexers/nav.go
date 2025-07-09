5c45edcb01aa425f2b51065cba7727d2bb9eb9c8
R Landau <rory.landau@gravwell.io>
2025-07-08 17:10:06 -0700

    junk redundant indexer stat action

--- gwcli/tree/systems/indexers/nav.go
+++ gwcli/tree/systems/indexers/nav.go
@@ -10,15 +10,11 @@
 package indexers
 
 import (
-	"github.com/gravwell/gravwell/v4/client/types"
 	"github.com/gravwell/gravwell/v4/gwcli/action"
-	"github.com/gravwell/gravwell/v4/gwcli/connection"
 	"github.com/gravwell/gravwell/v4/gwcli/stylesheet"
-	"github.com/gravwell/gravwell/v4/gwcli/utilities/scaffold/scaffoldlist"
 	"github.com/gravwell/gravwell/v4/gwcli/utilities/treeutils"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 )
 
 func NewIndexersNav() *cobra.Command {
@@ -34,80 +30,8 @@ func NewIndexersNav() *cobra.Command {
 	return treeutils.GenerateNav(use, short, long, aliases,
 		[]*cobra.Command{},
 		[]action.Pair{
-			newStatsListAction(),
 			get(),
 			list(),
 			newCalendarAction(),
 		})
 }
-
-//#region stats
-
-// wrapper for the SysStats map.
-// Allows us to omit a prefix for the embedded type.
-type wrappedSysStats struct {
-	Indexer               string
-	Uptime                uint64
-	TotalMemory           uint64
-	ProcessHeapAllocation uint64 // bytes allocated by this process's heap
-	ProcessSysReserved    uint64 // total bytes obtained from the OS
-	MemoryUsedPercent     float64
-	HostHash              string
-	Net                   types.NetworkUsage
-	VirtSystem            string          // e.g. "kvm" or "xen"
-	VirtRole              string          // "host" or "guest"
-	BuildInfo             types.BuildInfo // e.g. 3.3.1
-	CanonicalVersion      string
-	Iowait                float64
-}
-
-func newStatsListAction() action.Pair {
-	const (
-		use   string = "stats"
-		short string = "review the statistics of each indexer"
-		long  string = "Review the statistics of each indexer"
-	)
-
-	return scaffoldlist.NewListAction(
-		short, long,
-		wrappedSysStats{}, listStats, scaffoldlist.Options{
-			Use:            use,
-			Pretty:         nil, // TODO
-			DefaultColumns: []string{"Indexer", "Uptime", "TotalMemory", "VirtSystem", "VirtRole", "CanonicalVersion"},
-		})
-}
-
-func listStats(fs *pflag.FlagSet) ([]wrappedSysStats, error) {
-	var ns []wrappedSysStats
-
-	stats, err := connection.Client.GetSystemStats()
-	if err != nil {
-		return []wrappedSysStats{}, err
-	}
-	ns = make([]wrappedSysStats, len(stats))
-
-	// wrap the results
-	var i = 0
-	for k, v := range stats {
-		ns[i] = wrappedSysStats{
-			Indexer:               k,
-			Uptime:                v.Stats.Uptime,
-			TotalMemory:           v.Stats.TotalMemory,
-			ProcessHeapAllocation: v.Stats.ProcessHeapAllocation,
-			ProcessSysReserved:    v.Stats.ProcessSysReserved,
-			MemoryUsedPercent:     v.Stats.MemoryUsedPercent,
-			HostHash:              v.Stats.HostHash,
-			Net:                   v.Stats.Net,
-			VirtSystem:            v.Stats.VirtSystem,
-			VirtRole:              v.Stats.VirtRole,
-			BuildInfo:             v.Stats.BuildInfo,
-			CanonicalVersion:      v.Stats.BuildInfo.CanonicalVersion.String(),
-			Iowait:                v.Stats.Iowait,
-		}
-		i += 1
-	}
-
-	return ns, nil
-}
-
-//#endregion stats
