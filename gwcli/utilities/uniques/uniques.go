55e3a43d587674540261fda21d762db7b88d08c6
R Landau <rory.landau@gravwell.io>
2025-07-31 09:59:00 -0700

    reimplement walk for better maintainability

--- gwcli/utilities/uniques/uniques.go
+++ gwcli/utilities/uniques/uniques.go
@@ -15,6 +15,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"slices"
 	"strings"
 	"sync"
@@ -23,6 +24,7 @@ import (
 
 	"github.com/google/shlex"
 	"github.com/gravwell/gravwell/v4/gwcli/action"
+	"github.com/gravwell/gravwell/v4/gwcli/stylesheet"
 	ft "github.com/gravwell/gravwell/v4/gwcli/stylesheet/flagtext"
 	"github.com/gravwell/gravwell/v4/gwcli/utilities/cfgdir"
 	"github.com/spf13/cobra"
@@ -149,8 +151,8 @@ func AttachPersistentFlags(cmd *cobra.Command) {
 type WalkResult struct {
 	EndCmd          *cobra.Command // the last nav or action seen.
 	RemainingTokens []string       // all tokens remaining after endCmd
-	Builtin         string         // the non-help builtin to trigger
-	HelpMode        bool           // help was request for the named builtin or EndCmd (whichever is set)
+	Builtin         string         // the builtin to trigger; it will only contain "help" if HelpMode is also set (requesting help about help).
+	HelpMode        bool           // display help for the endCmd or builtin, rather than invoking it
 }
 
 // Walk traverses the given user input and returns how to handle it (and whether or not it is erroneous).
@@ -184,102 +186,109 @@ func Walk(pwd *cobra.Command, input string, builtinActions []string) (WalkResult
 	)
 	go func() {
 		defer wg.Done()
-		tokens, err = shlex.Split(input)
+		tokens, err = shlex.Split(strings.TrimSpace(input))
 	}()
 	wg.Wait()
 
 	if err != nil {
 		return WalkResult{}, err
+	} else if len(tokens) < 1 {
+		return WalkResult{
+			EndCmd: pwd,
+		}, nil
 	}
 
-	return innerWalk(pwd, slices.Clip(tokens), biSet, false)
+	// check for "help" mode or help flags on pwd
+	var helpMode bool
+	switch tokens[0] {
+	case "-h", "--help":
+		return WalkResult{EndCmd: pwd, HelpMode: true}, nil
+	case "help":
+		helpMode = true
+		// check for "help help"
+		if len(tokens[1:]) > 0 && tokens[1] == "help" {
+			return WalkResult{
+				EndCmd:          nil,
+				RemainingTokens: tokens[2:],
+				Builtin:         "help",
+				HelpMode:        true,
+			}, nil
+		}
+		tokens = tokens[1:]
+	}
+
+	endCmd, excessTokens, builtin, unknownToken := findEndCommand(pwd, slices.Clip(tokens), biSet)
+	// transform the results into a WalkResult
+	wr := WalkResult{
+		EndCmd:          endCmd,
+		RemainingTokens: excessTokens,
+		Builtin:         builtin,
+	}
+	// check for errors
+	if unknownToken != "" {
+		return wr, errors.New(unknownToken + " is not a valid child command of " + stylesheet.ColorCommandName(endCmd) + " or builtin")
+	} else if builtin == "help" {
+		// we explicitly check for help prior to findEndCommand.
+		// if it was found again, then this must have been bad input
+		return wr, fmt.Errorf("help must be of the form %v. See %v for more help",
+			stylesheet.Cur.ExampleText.Render("help "+ft.MutuallyExclusive([]string{"command path"})),
+			stylesheet.Cur.ExampleText.Render("help help"))
+	}
+	// look ahead for -h/--help
+	if slices.ContainsFunc(excessTokens, func(tkn string) bool { return tkn == "-h" || tkn == "--help" }) {
+		helpMode = true
+		// clip out the help flags so remaining tokens is consistent
+		wr.RemainingTokens = slices.DeleteFunc(excessTokens, func(tkn string) bool { return tkn == "-h" || tkn == "--help" })
+	}
+	wr.HelpMode = helpMode
+	return wr, nil
 }
 
-// innerWalk is the underlying, recursive driver for Walk.
+// findEndCommand is the underlying, recursive driver for Walk.
+// It traverses tokens to identify what nav, action, or builtin the user was attempting to invoke.
+// Stops on the first flag, action, or builtin it finds.
+//
 // pwd is our current position.
 // remainingTokens is the shlex'd tokens that have not yet been processed.
 // builtins is a hashset of builtin action names.
-// helpMode is set to true if the help builtin is found.
-func innerWalk(pwd *cobra.Command, remainingTokens []string, builtins map[string]bool, helpMode bool) (WalkResult, error) {
+//
+// end is the last valid cobra command found. It will always be populated.
+// excessTokens is extra tokens remaining post-traversal.
+// builtinInvoked is the name of the builtin to be invoked. Will be empty if the user did not invoke a builtin.
+// unknownToken is the non-flag token that stopped processing. Flags stop processing without returning unknown token.
+func findEndCommand(pwd *cobra.Command, remainingTokens []string, builtins map[string]bool) (end *cobra.Command, excessTokens []string, builtinInvoked string, unknownToken string) {
 	if len(remainingTokens) == 0 { // nothing left to parse, return current state
-		return WalkResult{
-			EndCmd:   pwd,
-			HelpMode: helpMode,
-		}, nil
+		return pwd, nil, "", ""
 	}
 	// cut the first token
 	curTkn, remainingTokens := strings.TrimSpace(remainingTokens[0]), remainingTokens[1:]
 	if curTkn == "" { // ignore extra whitespace
-		return innerWalk(pwd, remainingTokens, builtins, helpMode)
+		return findEndCommand(pwd, remainingTokens, builtins)
+	} else if curTkn[0] == '-' { // found a flag or flag-like token
+		return pwd, nil, "", ""
 	}
 	// special tokens have the highest priority
 	switch curTkn {
 	case "..": // up
-		return innerWalk(up(pwd), remainingTokens, builtins, helpMode)
+		return findEndCommand(up(pwd), remainingTokens, builtins)
 	case "~", "/": // root
-		return innerWalk(pwd.Root(), remainingTokens, builtins, helpMode)
+		return findEndCommand(pwd.Root(), remainingTokens, builtins)
 	}
 	// child commands have next highest priority
-	var nextCmd *cobra.Command
 	for _, child := range pwd.Commands() {
 		if child.Name() == curTkn || child.HasAlias(curTkn) {
-			nextCmd = child
-			break
-		}
-	}
-	if nextCmd != nil { // found a matching child
-		if action.Is(nextCmd) {
-			wr := WalkResult{
-				EndCmd:          nextCmd,
-				RemainingTokens: remainingTokens,
-				// if we are not already in help mode, check remaining tokens for -h/--help
-				HelpMode: helpMode || slices.ContainsFunc(remainingTokens, func(item string) bool { return item == "-h" || item == "--help" }),
+			if action.Is(child) {
+				return child, remainingTokens, "", ""
 			}
-			return wr, nil
+			// keep traversing navs
+			return findEndCommand(child, remainingTokens, builtins)
 		}
-		// navs keep walking so long as the next token is not -h/--help
-		if remainingTokens[0] == "-h" || remainingTokens[0] == "--help" {
-			return WalkResult{
-				EndCmd:          nextCmd,
-				RemainingTokens: remainingTokens,
-				Builtin:         "",
-				HelpMode:        true,
-			}, nil
-		}
-		return innerWalk(nextCmd, remainingTokens, builtins, helpMode)
 	}
-	// finally, check builtins
-	if curTkn == "help" { // special handling for "help"
-		// TODO help as the first token should be checked by Walk()
-		// if we find it again, this is bad input
-		// return "help keyword found multiple times"
-	}
-	/*if nextCmd == nil {
-		// NOTE(rlandau): remaining tokens does not (currently) include the erroneous token
-		return WalkResult{EndCmd: pwd, RemainingTokens: remainingTokens}, fmt.Errorf("%s is not a known child of %v", curTkn, pwd.Name())
-	}*/
-
-	// handle builtin commands
-	if curTkn == "help" { // help has special handling
-		// if helpMode is already set, then the user is requesting help about help
-		if helpMode {
-			return WalkResult{
-				EndCmd:   nil,
-				Builtin:  "help",
-				HelpMode: true,
-			}, nil
-		}
-		// check the next token for -h/--help
-		//if len(remainingTokens) > 0 && (remainingTokens[0] == "")
-
-	} else if _, found := builtins[curTkn]; found {
-		return WalkResult{
-			EndCmd:          pwd,
-			RemainingTokens: remainingTokens,
-			Builtin:         curTkn,
-		}, nil
+	if _, found := builtins[curTkn]; found {
+		return pwd, remainingTokens, curTkn, ""
 	}
 
+	return pwd, remainingTokens, "", curTkn
 }
 
 // Return the parent directory to the given command
@@ -288,6 +297,5 @@ func up(dir *cobra.Command) *cobra.Command {
 		return dir
 	}
 	// otherwise, step upward
-	//clilog.Writer.Debugf("Up: %v -> %v", dir.Name(), dir.Parent().Name())
 	return dir.Parent()
 }
