65563b38161776ad27132b731ee12cb89a6dd6fe
rory-landau-gravwell <rory.landau@gravwell.io>
2026-03-09 16:34:34 -0700

    don't bother to standardize titles

--- gwcli/utilities/scaffold/scaffoldcreate/create.go
+++ gwcli/utilities/scaffold/scaffoldcreate/create.go
@@ -118,12 +118,6 @@ func NewCreateAction(singular string, fields Config, createFunc CreateFuncT, ext
 		singular = "UNKNOWN"
 	}
 
-	// standardize field titles
-	for fn, f := range fields {
-		f.Title = strings.ToTitle(fields[fn].Title)
-		fields[fn] = f
-	}
-
 	// pull flags from provided fields
 	var flags = installFlagsFromFields(fields)
 	if extraFlagsFunc != nil {
