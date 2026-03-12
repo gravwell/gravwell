ed90b5185f3fba8a2327a4b97193e7fe0b368e7b
rory-landau-gravwell <rory.landau@gravwell.io>
2026-03-11 17:27:30 -0700

    clean up test to prepare for edit

--- gwcli/tree/resources/resources_noci_test.go
+++ gwcli/tree/resources/resources_noci_test.go
@@ -14,7 +14,6 @@ import (
 	"encoding/csv"
 	"os"
 	"path"
-	"slices"
 	"strconv"
 	"strings"
 	"testing"
@@ -29,6 +28,8 @@ const (
 	server             string = "localhost:8080"
 )
 
+var meta = []string{"--insecure", "-x", "-u", username, "--server=" + server}
+
 // Check that we can 1) create a new resource, 2) confirm we created that resource, and 3) download that resource
 func TestE2E(t *testing.T) {
 	tDir := t.TempDir()
@@ -37,8 +38,8 @@ func TestE2E(t *testing.T) {
 
 	// create a file to upload
 	var (
-		fileSize int64
-		filePath string
+		resourceSize int64
+		filePath     string
 	)
 	{
 		filePath = path.Join(tDir, t.Name()+"create.txt")
@@ -52,12 +53,12 @@ func TestE2E(t *testing.T) {
 		if err != nil {
 			t.Fatal()
 		}
-		fileSize = fi.Size()
+		resourceSize = fi.Size()
 		f.Close()
 	}
 
 	var (
-		resourceName   = randomdata.SillyName() + strconv.FormatInt(fileSize, 10)
+		resourceName   = randomdata.SillyName() + strconv.FormatInt(resourceSize, 10)
 		resourceDesc   = "from " + t.Name()
 		resourceLabels = []string{"lbl1", "otherlbl"}
 	)
@@ -74,74 +75,20 @@ func TestE2E(t *testing.T) {
 	}
 
 	// check that list pulls back the new resource
-	var resourceID string
-	{
-		resultPath := path.Join(tDir, t.Name()+"list.txt")
-		listResources := []string{"resources", "list",
-			"--csv",
-			"-o", resultPath,
-			//"--columns", "Name,ID,Size,Labels", // TODO once alias issue is resolved
-		}
-		// execute spins up singletons for us
-		if ec := tree.Execute(append(meta, listResources...)); ec != 0 {
-			t.Error("bad error code: ", ec)
-		}
-		// check for the macro we created
-		f, err := os.Open(resultPath)
-		if err != nil {
-			t.Error(err)
-		}
-		rdr := csv.NewReader(f)
-		rows, err := rdr.ReadAll()
-		if err != nil {
-			t.Fatal(err)
-		} else if len(rows) < 1 {
-			t.Fatal("no rows returned")
-		}
-		t.Log("resources:\n", rows, "\n")
-		// identify the Name column
-		nameColIdx := slices.Index(rows[0], "Name")
-		if nameColIdx == -1 {
-			t.Fatal("failed to identify \"Name\" column")
-		}
-		sizeColIdx := slices.Index(rows[0], "SizeBytes")
-		if sizeColIdx == -1 {
-			t.Fatal("failed to identify \"Size\" column")
-		}
-		idColIdx := slices.Index(rows[0], "ID")
-		if idColIdx == -1 {
-			t.Fatal("failed to identify \"ID\" column")
-		}
-		for i := 1; i < len(rows); i++ {
-			row := rows[i]
-			if row[nameColIdx] != resourceName {
-				continue
-			}
-			// sanity check size
-			reportedSize, err := strconv.ParseInt(row[sizeColIdx], 10, 64)
-			if err != nil {
-				t.Errorf("failed to parse %s into an int: %v", row[sizeColIdx], err)
-			}
-			if reportedSize != fileSize {
-				t.Fatal("incorrect size", testsupport.ExpectedActual(fileSize, reportedSize))
-			}
-			// sanity check labels
-			if lblsColIdx := slices.Index(rows[0], "Labels"); lblsColIdx == -1 {
-				t.Fatal("failed to identify \"Labels\" columns")
-			} else {
-				// parse and check the labels
-				col := row[lblsColIdx]
-				setLabels := strings.Split(strings.Trim(col, "[]"), " ") // slice off the brackets and split the labels into an array
-				if !testsupport.SlicesUnorderedEqual(setLabels, resourceLabels) {
-					t.Error("incorrect labels", testsupport.ExpectedActual(resourceLabels, setLabels))
-				}
-			}
-
-			resourceID = row[idColIdx]
-			break
-
-		}
+	resourceID, desc, lbls := listForItem(t, resourceName, resourceSize)
+	// validate
+	if desc != resourceDesc {
+		t.Error("retrieved incorrect description", testsupport.ExpectedActual(resourceDesc, desc))
 	}
+	if !testsupport.SlicesUnorderedEqual(lbls, resourceLabels) {
+		t.Error("assigned labels do not match given labels", testsupport.ExpectedActual(resourceLabels, lbls))
+	}
+
+	// check that we can alter one of the properties
+	{
+
+	}
+
 	// check that we can download the resource
 	{
 		resultPath := filePath + ".redown.txt"
@@ -164,4 +111,60 @@ func TestE2E(t *testing.T) {
 		}
 	}
 
+}
+
+// listForItem executes "list", identifies a row with the given name, and returns its details.
+func listForItem(t *testing.T, name string, size int64) (id, description string, labels []string) {
+	resultPath := path.Join(t.TempDir(), t.Name()+"list.txt")
+	// execute spins up singletons for us
+	if ec := tree.Execute(append(meta, []string{"resources", "list",
+		"--csv",
+		"-o", resultPath,
+		"--columns", "CommonFields.ID,CommonFields.Name,CommonFields.Description,Size,CommonFields.Labels",
+	}...)); ec != 0 {
+		t.Error("bad error code: ", ec)
+	}
+	// slurp the file we wrote to
+	var rows [][]string
+	{
+		f, err := os.Open(resultPath)
+		if err != nil {
+			t.Error(err)
+		}
+		rdr := csv.NewReader(f)
+		rows, err = rdr.ReadAll()
+		if err != nil {
+			t.Fatal(err)
+		} else if len(rows) < 1 {
+			t.Fatal("no rows returned")
+		}
+	}
+	t.Log("columns:\n", rows[0], "\n")
+	if len(rows[0]) != 5 {
+		t.Fatal("incorrect column count", testsupport.ExpectedActual(5, len(rows[0])))
+	}
+	for i := 1; i < len(rows); i++ {
+		row := rows[i]
+
+		// check if this our row
+		if row[1] != name {
+			continue
+		}
+		// validate size
+		reportedSize, err := strconv.ParseInt(row[3], 10, 64)
+		if err != nil {
+			t.Errorf("failed to parse %s into an int: %v", row[2], err)
+		}
+		if reportedSize != size {
+			t.Fatal("incorrect size", testsupport.ExpectedActual(size, reportedSize))
+		}
+		// fetch data to return
+		id = row[0]
+		description = row[2]
+		labels = strings.Split(strings.Trim(row[4], "[]"), " ") // slice off the brackets and split the labels into an array
+
+		return id, description, labels
+	}
+	t.Fatalf("found no rows with name %v. Rows: %v", name, rows[1:])
+	return
 }
