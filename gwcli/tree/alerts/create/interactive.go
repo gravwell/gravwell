627aa55d20f9555577a868e66fea48975a8b2e7b
rory-landau-gravwell <rory.landau@gravwell.io>
2026-03-19 17:02:00 -0700

    WIP implement multi-select list handling

--- gwcli/tree/alerts/create/interactive.go
+++ gwcli/tree/alerts/create/interactive.go
@@ -2,6 +2,7 @@ package alertscreate
 
 import (
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/charmbracelet/bubbles/list"
@@ -31,18 +32,15 @@ const (
 type createModel struct {
 	// stages
 	stage            stage
-	stageDispatchers struct {
-		m list.Model
-	}
-	consumersModel list.Model
-	metadata       metadata
+	dispatchersModel list.Model
+	consumersModel   list.Model
+	metadata         *metadata
 }
 
 func newCreateModel() *createModel {
 	return &createModel{
-		// availDispatcher/availConsumer are allocated and set in SetArgs()
-
-		// stages are built in SetArgs as they need to gather data
+		metadata: NewMetadata(),
+		// list models are generated in SetArgs
 	}
 }
 
@@ -52,16 +50,41 @@ func (c *createModel) Init() tea.Cmd {
 }
 
 func (c *createModel) Update(msg tea.Msg) tea.Cmd {
-	var cmd tea.Cmd
+	var retCmd tea.Cmd
 	switch c.stage {
 	case stageDispatchers:
-		c.stageDispatchers.m, cmd = c.stageDispatchers.m.Update(msg)
+		if keyMsg, ok := msg.(tea.KeyMsg); ok {
+			switch keyMsg.Type {
+			case tea.KeyEnter:
+				c.stage = stageConsumers
+				return nil
+			case tea.KeySpace:
+				li, ok := c.dispatchersModel.SelectedItem().(item)
+				if !ok {
+					clilog.Writer.Errorf("failed to cast dispatcher from item. Bare item: %v", li)
+					return nil
+				}
+				li.Selected = !li.Selected
+				// reinsert the item
+
+				cmd := c.dispatchersModel.SetItem(c.dispatchersModel.GlobalIndex(), li)
+
+				var statusMsg string
+				if li.Selected {
+					statusMsg = "selected"
+				} else {
+					statusMsg = "deselected"
+				}
+				statusMsg += " dispatcher " + li.Title()
+				return tea.Batch(cmd, c.dispatchersModel.NewStatusMessage(statusMsg))
+			}
+		}
+
+		c.dispatchersModel, retCmd = c.dispatchersModel.Update(msg)
 	case stageConsumers:
-		c.consumersModel, cmd = c.consumersModel.Update(msg)
+		c.consumersModel, retCmd = c.consumersModel.Update(msg)
 	case stageMetadata:
-		var trySubmit bool
-		cmd, trySubmit = c.metadata.Update(msg)
-		if trySubmit {
+		if cmd, trySubmit, backToDispatchers, backToConsumers := c.metadata.Update(msg); trySubmit {
 			// coalesce all of our data into an alert definition
 			var maxEvents int
 			if str := c.metadata.maxEvents.Value(); str != "" {
@@ -83,7 +106,7 @@ func (c *createModel) Update(msg tea.Msg) tea.Cmd {
 			}
 
 			dispatchers := []types.AlertDispatcher{}
-			for _, li := range c.stageDispatchers.m.Items() {
+			for _, li := range c.dispatchersModel.Items() {
 				dsp, ok := li.(item)
 				if !ok {
 					clilog.Writer.Errorf("failed to cast dispatcher from item. Bare item: %v", li)
@@ -123,20 +146,26 @@ func (c *createModel) Update(msg tea.Msg) tea.Cmd {
 				c.metadata.submitErr = err.Error()
 				return nil
 			}
-
+			c.stage = quitting
 			return tea.Println(phrases.SuccessfullyCreatedItem("alert", res.GUID.String()))
+		} else if backToDispatchers {
+			c.stage = stageDispatchers
+		} else if backToConsumers {
+			c.stage = stageConsumers
+		} else {
+			retCmd = cmd
 		}
 	case quitting:
 		return nil
 	}
 
-	return cmd
+	return retCmd
 }
 
 func (c *createModel) View() string {
 	switch c.stage {
 	case stageDispatchers:
-		return c.stageDispatchers.m.View()
+		return c.dispatchersModel.View()
 	case stageConsumers:
 		return c.consumersModel.View()
 	case stageMetadata:
@@ -153,8 +182,12 @@ func (c *createModel) Done() bool {
 
 func (c *createModel) Reset() error {
 	c.stage = stageDispatchers
+
 	// empty out the structs
-	// list models will be reset in SetArgs
+
+	// list models will be rebuilt on the next SetArgs
+	c.dispatchersModel = list.Model{}
+	c.consumersModel = list.Model{}
 	c.metadata.Reset()
 	return nil
 }
@@ -182,30 +215,50 @@ func (c *createModel) SetArgs(_ *pflag.FlagSet, tokens []string, width, height i
 		return "", tea.Println(phrases.SuccessfullyCreatedItem("alert", res.ThingUUID.String())), nil
 	}
 
-	// push dispatchers and consumers into their respective lists by wrapping each entry as an item
+	// push dispatchers into their respective lists by wrapping each entry as an item
+	var wg sync.WaitGroup
+
 	dispatchers := make([]list.Item, len(availDispatchers))
-	var i int
-	for _, dsp := range availDispatchers {
-		dispatchers[i] = item{
-			Name: dsp.Name,
-			Desc: dsp.Description,
-			GUID: dsp.GUID,
+	wg.Go(func() {
+		// if the user pre-selected dispatchers, make sure they are selected in the list
+		selected := make(map[uuid.UUID]bool, len(flagVals.dispatcherIDs))
+		for _, uid := range flagVals.dispatcherIDs {
+			selected[uid] = true
 		}
-		i += 1
-	}
-	c.stageDispatchers = struct{ m list.Model }{
-		m: list.New(dispatchers, list.NewDefaultDelegate(), width, height),
-	}
-	i = 0
+		var i int
+		for _, dsp := range availDispatchers {
+			dispatchers[i] = item{
+				Name:     dsp.Name,
+				Desc:     dsp.Description,
+				GUID:     dsp.GUID,
+				Selected: selected[dsp.GUID],
+			}
+			i += 1
+		}
+		c.dispatchersModel = list.New(dispatchers, list.NewDefaultDelegate(), width, height)
+		c.dispatchersModel.StatusMessageLifetime = stylesheet.StatusMessageLifetime
+	})
+
 	consumers := make([]list.Item, len(availConsumers))
-	for _, dsp := range availConsumers {
-		consumers[i] = item{
-			Name: dsp.Name,
-			Desc: dsp.Description,
-			GUID: dsp.GUID,
+	wg.Go(func() {
+		// if the user pre-selected dispatchers, make sure they are selected in the list
+		selected := make(map[uuid.UUID]bool, len(flagVals.consumerGUIDs))
+		for _, uid := range flagVals.consumerGUIDs {
+			selected[uid] = true
 		}
-		i += 1
-	}
+		var i int
+		for _, cns := range availConsumers {
+			consumers[i] = item{
+				Name:     cns.Name,
+				Desc:     cns.Description,
+				GUID:     cns.GUID,
+				Selected: selected[cns.GUID],
+			}
+			i += 1
+		}
+	})
+	wg.Wait()
+
 	// prepopulate data
 	c.consumersModel = list.New(consumers, list.NewDefaultDelegate(), width, height)
 	c.metadata.Init(flagVals.name, flagVals.description, flagVals.tag, flagVals.enabled, flagVals.maxEvents, flagVals.retain)
