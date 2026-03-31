9516ad10ba4683b305d8d5732fd7f0d47aa3628a
John Floren <john@jfloren.net>
2026-03-31 09:39:48 -0700

    Remove deprecated Error and ErrorNode fields in FlowParseResponse
    It's 6.0, when all contracts are broken and mere chaos is unleashed
    upon the codebase.

--- client/flows.go
+++ client/flows.go
@@ -9,7 +9,6 @@
 package client
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/gravwell/gravwell/v4/client/types"
@@ -78,35 +77,25 @@ func (c *Client) UpdateFlow(ss types.Flow) error {
 	return c.putStaticURL(flowIdUrl(ss.ID), ss)
 }
 
-// ParseFlow asks the API to check a flow.
-// If there is no error, outputPayloads will be a map containing the outputs
-// of each node, keyed by the node ID.
-func (c *Client) ParseFlow(flow string) (outputPayloads map[int]map[string]interface{}, err error) {
-	var resp types.FlowParseResponse
+// ParseFlow asks the API to check a flow.  It will only return error
+// if there was a problem submitting the request; check the OK field
+// of the FlowParseResponse to see if the parse succeeded or not.
+func (c *Client) ParseFlow(flow string) (resp types.FlowParseResponse, err error) {
 	req := types.FlowParseRequest{
 		Flow: flow,
 	}
 	if err = c.methodStaticPushURL(http.MethodPut, flowParseUrl(), req, &resp, nil, nil); err != nil {
 		return
 	}
-
-	//if the parse failed but we don't have an error, set something
-	if !resp.OK {
-		if len(resp.Error) == 0 {
-			resp.Error = `Unknown parse error`
-		}
-		err = errors.New(resp.Error)
-	}
-	outputPayloads = resp.OutputPayloads
 	return
 }
 
 // ParseReactiveFlow asks the API to check a flow as if triggered by an alert.
 // The event parameter will be injected into the initial payload under the name `event`.
-// If there is no error, outputPayloads will be a map containing the outputs
-// of each node, keyed by the node ID.
-func (c *Client) ParseReactiveFlow(flow string, event types.Event) (outputPayloads map[int]map[string]interface{}, err error) {
-	var resp types.FlowParseResponse
+// It will only return error
+// if there was a problem submitting the request; check the OK field
+// of the FlowParseResponse to see if the parse succeeded or not.
+func (c *Client) ParseReactiveFlow(flow string, event types.Event) (resp types.FlowParseResponse, err error) {
 	req := types.FlowParseRequest{
 		DebugEvent: &event,
 		Flow:       flow,
@@ -114,15 +103,6 @@ func (c *Client) ParseReactiveFlow(flow string, event types.Event) (outputPayloa
 	if err = c.methodStaticPushURL(http.MethodPut, flowParseUrl(), req, &resp, nil, nil); err != nil {
 		return
 	}
-
-	//if the parse failed but we don't have an error, set something
-	if !resp.OK {
-		if len(resp.Error) == 0 {
-			resp.Error = `Unknown parse error`
-		}
-		err = errors.New(resp.Error)
-	}
-	outputPayloads = resp.OutputPayloads
 	return
 }
 
