b2d68fc3d609b7187cf94cd109c14dda1699f592
rory-landau-gravwell <rory.landau@gravwell.io>
2026-03-31 17:00:37 -0700

    switch scaffoldbasic over to CommonOptions

--- gwcli/utilities/scaffold/options.go
+++ gwcli/utilities/scaffold/options.go
@@ -17,26 +17,12 @@ import (
 )
 
 type BasicOptions struct {
-	// AddtlFlagFunc provides a function that defines additional flags for this specific action.
-	// These flags will be added to the standard flagset of a basic action.
-	AddtlFlagFunc func() pflag.FlagSet
-	// Other names for this action.
-	Aliases []string
+	CommonOptions
+
 	// Free-form function called in SetArgs or at the start of run to validate the given flags.
 	// Called after the cmd's .Args() function (if !nil and !err).
 	// You can assume that the flags have already been parsed, but that no additional actions have been taken on them.
 	ValidateArgs func(*pflag.FlagSet) (invalid string, err error)
-
-	//#region command functions
-
-	// Override the default usage line printed as part of `help`/`-h`.
-	Usage string
-
-	// Provide an example of using this action. Should start with the action's name.
-	Example string
-
-	//#endregion command functions
-
 }
 
 // CommonOptions are options that span all scaffolds and thus should be applied the same for each.
