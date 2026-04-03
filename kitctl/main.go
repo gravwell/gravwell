0b0f0f3f5dcd731fce384a74df507f5471906b54
rory-landau-gravwell <rory.landau@gravwell.io>
2026-04-03 13:05:13 -0700

    match PackedFile to PackedResource

--- kitctl/main.go
+++ kitctl/main.go
@@ -474,7 +474,7 @@ func packKit(args []string) {
 				log.Fatal(err)
 			}
 		case kits.File:
-			var x types.FileFull
+			var x kits.PackedFile
 			if x, err = readFile(wd, itm.Name); err != nil {
 				log.Fatalf("Could not read %v %v: %v", itm.Type.String(), itm.Name, err)
 			}
@@ -783,12 +783,16 @@ func unpackKitItems(wd string, rdr *kits.Reader) error {
 				return fmt.Errorf("Failed to write out %v %v: %v", tp.String(), name, err)
 			}
 		case kits.File:
-			var p types.FileFull
-			if err = json.NewDecoder(rdr).Decode(&p); err != nil {
-				return fmt.Errorf("Failed to decode %v %v: %v", tp.String(), name, err)
+			var pf kits.PackedFile
+			if err = json.NewDecoder(rdr).Decode(&pf); err != nil {
+				return fmt.Errorf("Failed to decode file %v: %v", name, err)
 			}
-			if err := writeFile(wd, name, p); err != nil {
-				return fmt.Errorf("Failed to write out %v %v: %v", tp.String(), name, err)
+			pf.Name = name
+			if err = pf.Validate(); err != nil {
+				return fmt.Errorf("Failed to validate file %v: %v", name, err)
+			}
+			if err := writeFile(wd, pf); err != nil {
+				return fmt.Errorf("Failed to write out file %v: %v", name, err)
 			}
 		case kits.SearchLibrary:
 			var p types.SavedQuery
