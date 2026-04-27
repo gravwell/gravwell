fa99d77f0fa29145182df178ee2806c891e8f8a4
rory-landau-gravwell <rory.landau@gravwell.io>
2026-04-27 12:33:59 -0700

    fix tests

--- gwcli/connection/errors.go
+++ gwcli/connection/errors.go
@@ -3,9 +3,13 @@ package connection
 import "errors"
 
 var (
-	ErrAPITokenRequired   error = errors.New("MFA is enabled, API token is required")
-	ErrNotInitialized     error = errors.New("client must be initialized")
-	ErrAPITokenInvalid    error = errors.New("API key could not be validated")
-	ErrMFASetupRequired   error = errors.New("MFA is required. Please log in via the browser to set it up.")
-	ErrInvalidCredentials error = errors.New("failed to authenticate with the given credentials")
+	ErrAPITokenRequired                     error = errors.New("MFA is enabled, API token is required")
+	ErrNotInitialized                       error = errors.New("client must be initialized")
+	ErrAPITokenInvalid                      error = errors.New("API key could not be validated")
+	ErrMFASetupRequired                     error = errors.New("MFA is required. Please log in via the browser to set it up.")
+	ErrInvalidCredentials                   error = errors.New("failed to authenticate with the given credentials")
+	ErrNonInteractiveRequiresDifferentLogin error = errors.New("non-interactive mode requires one of the following login methods:\n" +
+		"1) explicit username and password (-u)\n" +
+		"2) an API token (--api/--eapi)\n" +
+		"3) or a valid session from a prior, successful login")
 )
