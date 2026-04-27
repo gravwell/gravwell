1638bd55a0562e6a84002429e2d17d9d6ccd30e3
rory-landau-gravwell <rory.landau@gravwell.io>
2026-04-27 08:24:13 -0700

    Implement unique ID type into MSL

--- gwcli/stylesheet/multiselectlist/multiselectlist.go
+++ gwcli/stylesheet/multiselectlist/multiselectlist.go
@@ -15,8 +15,8 @@ package multiselectlist
 
 import (
 	"fmt"
+	"maps"
 	"slices"
-	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -25,7 +25,7 @@ import (
 )
 
 // Model bolts additional functionality onto the list bubble such that multiple items can be selected.
-type Model struct {
+type Model[ID_t comparable] struct {
 	list.Model
 
 	done bool
@@ -37,15 +37,67 @@ type Model struct {
 }
 
 type Options struct {
-	// if not nil, items with matching titles will be preselected.
-	Preselected map[uint]bool
-	// sets the prefix to show whether or not an item is selected.
+	// Returns a string to be prefixed to each item in the list to show it's selected state.
+	//
 	// Uses DefaultSelectedViewFunc if nil.
-	SelectedViewFunc func(set bool) string
+	ShowSelectStateViewFunc func(selected bool) string
 }
 
-// DefaultSelectedViewFunc sets the prefix if Options.SelectedViewFunc is not set.
-func DefaultSelectedViewFunc(set bool) string {
+//#region Item Wrappers
+
+type SelectableItem[ID_t comparable] interface {
+	// FilterValue is the value we use when filtering against this item when
+	// we're filtering the list.
+	FilterValue() string
+	Title() string
+	// unique identifier for this item
+	ID() ID_t
+	Description() string
+	// Toggle the selection state of this item.
+	SetSelected(selected bool)
+	// Is this item currently selected?
+	Selected() bool
+}
+
+var _ SelectableItem[any] = &DefaultSelectableItem[any]{}
+
+// DefaultSelectableItem provides a struct for use in the SelectableItem interface for users that do not wish to customize at all.
+type DefaultSelectableItem[ID_t any] struct {
+	Ttl        string
+	Desc       string
+	Slctd      bool
+	Identifier ID_t
+}
+
+// FilterValue filters on the concat of ttl and desc.
+func (i DefaultSelectableItem[ID_t]) FilterValue() string {
+	return i.Ttl + i.Desc
+}
+
+func (i DefaultSelectableItem[ID_t]) Title() string {
+	return i.Ttl
+}
+
+func (i DefaultSelectableItem[ID_t]) ID() ID_t {
+	return i.Identifier
+}
+
+func (i DefaultSelectableItem[ID_t]) Description() string {
+	return i.Desc
+}
+
+func (i *DefaultSelectableItem[ID_t]) SetSelected(selected bool) {
+	i.Slctd = selected
+}
+
+func (i DefaultSelectableItem[ID_t]) Selected() bool {
+	return i.Slctd
+}
+
+//#endregion Item Wrappers
+
+// DefaultShowSelectStateView sets the prefix if Options.ShowSelectStateViewFunc is not set.
+func DefaultShowSelectStateView(set bool) string {
 	if set {
 		return "[✓]"
 	}
@@ -53,36 +105,30 @@ func DefaultSelectedViewFunc(set bool) string {
 }
 
 // New returns a Multi-Select enabled list with the default delegate used by list.
-func New(items []list.DefaultItem, width, height int, opts Options) Model {
-	// make sure the pre-selection map isn't nil
-	if opts.Preselected == nil {
-		opts.Preselected = make(map[uint]bool)
+func New[ID_t comparable](items []SelectableItem[ID_t], width, height int, opts Options) Model[ID_t] {
+	svf := DefaultShowSelectStateView
+	if opts.ShowSelectStateViewFunc != nil {
+		svf = opts.ShowSelectStateViewFunc
 	}
 
-	svf := DefaultSelectedViewFunc
-	if opts.SelectedViewFunc != nil {
-		svf = opts.SelectedViewFunc
-	}
-
-	msl := Model{
-		Model:            list.New(wrapItems(items, svf, opts.Preselected), list.NewDefaultDelegate(), width, height),
+	msl := Model[ID_t]{
+		Model:            list.New(wrapItems(items), list.NewDefaultDelegate(), width, height),
 		selectedViewFunc: svf,
 	}
 
 	return msl
 }
 
-func wrapItems(bareItems []list.DefaultItem, svf func(set bool) string, preselected map[uint]bool) []list.Item {
+func wrapItems[ID_t comparable](bareItems []SelectableItem[ID_t]) []list.Item {
 	// wrap each item in our select-enabled item type
 	wrapped := make([]list.Item, len(bareItems))
 	for i, item := range bareItems {
-		wrapped[i] = selectableItem{
-			item, preselected[uint(i)], svf}
+		wrapped[i] = item
 	}
 	return wrapped
 }
 
-func (msl Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+func (msl Model[ID_t]) Update(msg tea.Msg) (Model[ID_t], tea.Cmd) {
 	if msg, ok := msg.(tea.WindowSizeMsg); ok {
 		msl.Model.SetHeight(msg.Height)
 		msl.Model.SetWidth(msg.Width)
@@ -103,35 +149,64 @@ func (msl Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	return msl, cmd
 }
 
-func (msl Model) View() string {
+func (msl Model[ID_t]) View() string {
 	return msl.Model.View() + "\n  " + stylesheet.Cur.DisabledText.Render("space select • ↲ continue")
+}
+
+// CursorItem returns the item at the current cursor.
+func (msl *Model[ID_t]) CursorItem() SelectableItem[ID_t] {
+	return msl.Item(msl.Index(), false)
+}
+
+// Item fetches the item at the given index.
+//
+// If global, index ignores current filter state.
+//
+// Returns nil if not found.
+func (msl *Model[ID_t]) Item(index int, global bool) SelectableItem[ID_t] {
+	if index < 0 {
+		return nil
+	}
+
+	var items []list.Item
+	if global {
+		items = msl.Items()
+	} else {
+		items = msl.VisibleItems()
+	}
+
+	if len(items) == 0 || len(items) <= index {
+		return nil
+	}
+
+	sel, ok := items[index].(SelectableItem[ID_t])
+	if !ok {
+		panicFailedAssert(items[index])
+	}
+	return sel
 }
 
 // ToggleCurrentItem does as it says on the tin.
 // If no item is selected (aka the list is empty or your cursor is off in wonderland), this is a no-op.
 //
 // NOTE(rlandau): This function can panic, but if it does, something has gone truly, horrifically wrong.
-func (msl *Model) ToggleCurrentItem() tea.Cmd {
-	baseItem := msl.Model.SelectedItem()
-	if baseItem == nil {
+func (msl *Model[ID_t]) ToggleCurrentItem() tea.Cmd {
+	item := msl.CursorItem()
+	if item == nil {
 		return nil
 	}
-	li, ok := baseItem.(selectableItem)
-	if !ok {
-		panicFailedAssert(msl.Model.SelectedItem())
-	}
-	li.selected = !li.selected
+	item.SetSelected(!item.Selected())
 	// reinsert the item
-	cmd := msl.Model.SetItem(msl.Model.GlobalIndex(), li)
+	cmd := msl.Model.SetItem(msl.Model.GlobalIndex(), item)
 
 	if msl.StatusMessageOnSelect {
 		var statusMsg string
-		if li.selected {
+		if item.Selected() {
 			statusMsg = "selected"
 		} else {
 			statusMsg = "deselected"
 		}
-		statusMsg += " " + li.Title()
+		statusMsg += " " + item.Title()
 		cmd = tea.Batch(cmd, msl.Model.NewStatusMessage(statusMsg))
 	}
 
@@ -141,95 +216,71 @@ func (msl *Model) ToggleCurrentItem() tea.Cmd {
 
 // Done returns true once the user hits enter.
 // It should be checked after each msl.Update()
-func (msl *Model) Done() bool {
+func (msl *Model[ID_t]) Done() bool {
 	return msl.done
 }
 
 // Undone unsets the done flag without resetting the whole model.
-func (msl *Model) Undone() {
+func (msl *Model[ID_t]) Undone() {
 	msl.done = false
 }
 
 // GetSelectedItems returns the list of selected items.
 //
 // Operates in O(n) time where n = len(msl.Items()).
-func (msl *Model) GetSelectedItems() []list.DefaultItem {
+func (msl *Model[ID_t]) GetSelectedItems() []SelectableItem[ID_t] {
 	items := msl.Model.Items()
-	sel := make([]list.DefaultItem, 0, len(items))
+	// cast items back to Selectable
+	sel := make([]SelectableItem[ID_t], 0, len(items))
 	for _, item := range items {
-		selectable, ok := item.(selectableItem)
+		selectable, ok := item.(SelectableItem[ID_t])
 		if !ok {
 			panicFailedAssert(item)
 		}
-		if selectable.selected {
-			sel = append(sel, selectable.DefaultItem)
+		if selectable.Selected() {
+			sel = append(sel, selectable)
 		}
 
 	}
 	return slices.Clip(sel)
 }
 
-// SelectItems selects each item with a title in toSelect.
-// Returns the first item with no matches.
+// SelectItems selects each item with a .ID that matches one of them items in toSelect.
+// Returns the first ID with no matches.
 //
-// Operates in O(toSelect * len(msl)) time.
-func (msl *Model) SelectItems(toSelect []string) (cmd tea.Cmd, notFound string) {
-	// TODO when we replace DefaultList, would be nice to have a UniqueIdentifier field in the delegate we match against instead.
+// Operates in roughly O(toSelect * len(msl)) time.
+func (msl *Model[ID_t]) SelectItems(toSelect []ID_t) (cmd tea.Cmd, notFound []ID_t) {
 
+	// duplicate toSelect and remove an item whenever it is found
+	var nf = map[ID_t]bool{}
+	for _, id := range toSelect {
+		nf[id] = true
+	}
 	var cmds []tea.Cmd
 	itms := msl.Items()
-	for _, sel := range toSelect {
-		found := false
+	for _, id := range toSelect {
 		for i, itm := range itms {
-			selectable, ok := itm.(selectableItem)
+			selectable, ok := itm.(SelectableItem[ID_t])
 			if !ok {
 				panicFailedAssert(itm)
 			}
-			if strings.ToLower(selectable.Title()) == sel {
-				selectable.selected = true
+			if selectable.ID() == id {
+				selectable.SetSelected(true)
 				// reinsert the item
 				cmds = append(cmds, msl.Model.SetItem(i, selectable))
-				found = true
+				delete(nf, id)
 			}
 		}
-		if !found {
-			return nil, sel
-		}
 	}
-	return tea.Batch(cmds...), ""
+	return tea.Batch(cmds...), slices.Collect(maps.Keys(nf))
 }
 
-func (msl *Model) SetItems(items []list.DefaultItem, preselect map[uint]bool) tea.Cmd {
-	wrapped := wrapItems(items, msl.selectedViewFunc, preselect)
+func (msl *Model[ID_t]) SetItems(items []SelectableItem[ID_t]) tea.Cmd {
+	wrapped := wrapItems(items)
 	return msl.Model.SetItems(wrapped)
 }
 
-//#region selectable item
-
-// selectableItem wraps a given item type, prefixing select functionality
-type selectableItem struct {
-	list.DefaultItem
-	selected         bool
-	selectedViewFunc func(set bool) string
-}
-
-// FilterValue sets the string to include/disclude this item on when a user filters.
-func (i selectableItem) FilterValue() string {
-	return i.DefaultItem.FilterValue()
-}
-
-func (i selectableItem) Title() string {
-	return i.selectedViewFunc(i.selected) + " " + i.DefaultItem.Title()
-}
-
-func (i selectableItem) Description() string {
-	return i.DefaultItem.Description()
-}
-
-//#endregion selectable item
-
-// assertTarget should be "selectableItem" or "list.DefaultItem"
 // pray this is never called.
 func panicFailedAssert(baseItem list.Item) {
-	panic(fmt.Sprintf("failed to cast item to selectableItem. Base item: %#v", baseItem))
+	panic(fmt.Sprintf("failed to cast item to SelectableItem. Base item: %#v", baseItem))
 }
