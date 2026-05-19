b272b6a06faa9ffd341f26955ffdb2d17113b5af
Tyler Elton <tyler.elton@gravwell.io>
2026-05-19 14:23:30 -0400

    chore: add doc comment

--- hosted/plugins/msgraph/msgraph.go
+++ hosted/plugins/msgraph/msgraph.go
@@ -5,6 +5,9 @@
  * This software may be modified and distributed under the terms of the
  * BSD 2-clause license. See the LICENSE file for details.
  **************************************************************************/
+
+// Package msgraph provides a shared MS Graph Security API client and a hosted
+// ingester plugin for the Gravwell hosted ingester runtime.
 package msgraph
 
 import (
