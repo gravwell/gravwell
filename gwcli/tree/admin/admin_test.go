550a9ca7ccea0855fbbbd180863b449bdf1a0b6d
rory-landau-gravwell <rory.landau@gravwell.io>
2026-05-19 08:52:27 -0700

    add header

--- gwcli/tree/admin/admin_test.go
+++ gwcli/tree/admin/admin_test.go
@@ -1,3 +1,13 @@
+//go:build noci
+
+/*************************************************************************
+ * Copyright 2026 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD 2-clause license. See the LICENSE file for details.
+ **************************************************************************/
+
 package admin_test
 
 import (
