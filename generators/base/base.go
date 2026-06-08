5b28b18317681432c6c6384f6ae29bc4a0dfc54e
kris <kris.watts@gravwell.io>
2026-06-07 22:08:17 -0600

    add comment to generator and max entry size

--- generators/base/base.go
+++ generators/base/base.go
@@ -50,7 +50,7 @@ var (
 	chaos        = flag.Bool("chaos-mode", false, "Chaos mode causes the generator to not do multiline HTTP uploads and sometimes send crazy timestamps")
 	chaosWorkers = flag.Int("chaos-mode-workers", 8, "Maximum number of workers when in chaos mode")
 	tsPsychoMode = flag.Bool("time-is-an-illusion", false, "Ingest with worst-case timestamp ordering (this is a chaos-mode flag)")
-	maxEntrySize = flag.Int("max-entry-size", 16*1024*1024, "Maximum entry size to limit to")
+	maxEntrySize = flag.Int("max-entry-size", 32*1024*1024, "Maximum entry size to limit to") // set to something dumb just so that we know megaJSON will work
 )
 
 var (
