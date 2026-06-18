d7a1ee8a4254fe760f5d714b1a651ff2ddb8fb88
Nic O'Sullivan <nic.osullivan@gravwell.io>
2026-06-18 16:57:24 -0500

    add raw hec ingest test

--- e2e/ingesters/HttpIngester/hec_test.go
+++ e2e/ingesters/HttpIngester/hec_test.go
@@ -14,48 +14,56 @@ import (
 func TestHec(t *testing.T) {
 	_, endpoint := setup(t, "hec")
 
-	tests := []struct {
-		name string
-		t    func(t *testing.T)
-	}{
-		{
-			name: "test basic ingest",
-			t: func(t *testing.T) {
-				data := `{"event": "hec blah"}`
-				SendHec(t, endpoint, strings.NewReader(data))
+	t.Run("event ingest", func(t *testing.T) {
+		data := `{"event": "hec blah"}`
+		SendHecEvent(t, endpoint, strings.NewReader(data))
 
-				c := e2e.GetClient(t)
-				ent := e2e.WaitForEntries(t, c, "tag=hec-testing", time.Minute, 1, 30*time.Second)
-				if len(ent) != 1 {
-					e2e.Fatalf(t, "got %d entries, want 1", len(ent))
-				}
-				if string(ent[0].Data) != "hec blah" {
-					e2e.Fatalf(t, "got %s, want %s", string(ent[0].Data), data)
-				}
-			},
-		},
-		{
-			name: "ensure debug posts",
-			t: func(t *testing.T) {
-				data := `{"event": "hec blah blah"}`
-				SendHec(t, endpoint, strings.NewReader(data))
+		c := e2e.GetClient(t)
+		ent := e2e.WaitForEntries(t, c, "tag=hec-testing", time.Minute, 1, 30*time.Second)
+		if len(ent) != 1 {
+			e2e.Fatalf(t, "got %d entries, want 1", len(ent))
+		}
+		if string(ent[0].Data) != "hec blah" {
+			e2e.Fatalf(t, "got %s, want %s", string(ent[0].Data), "hec blah")
+		}
+	})
 
-				c := e2e.GetClient(t)
-				ent := e2e.WaitForEntries(t, c, fmt.Sprintf(`tag=gravwell syslog Appname==httpingester Message=="HEC request" bytes==%d`, len(data)), time.Minute, 1, 30*time.Second)
-				if len(ent) != 1 {
-					e2e.Fatalf(t, "got %d entries, want 1", len(ent))
-				}
-			},
-		},
-	}
+	t.Run("raw ingest", func(t *testing.T) {
+		data := `raw hec`
+		SendHecRaw(t, endpoint, strings.NewReader(data))
 
-	for _, test := range tests {
-		t.Run(test.name, test.t)
-	}
+		c := e2e.GetClient(t)
+		ent := e2e.WaitForEntries(t, c, "tag=hec-testing words -e DATA raw hec", time.Minute, 1, 30*time.Second)
+		if len(ent) != 1 {
+			e2e.Fatalf(t, "got %d entries, want 1", len(ent))
+		}
+		if string(ent[0].Data) != data {
+			e2e.Fatalf(t, "got %s, want %s", string(ent[0].Data), data)
+		}
+	})
+
+	t.Run("debug posts", func(t *testing.T) {
+		data := `{"event": "hec blah blah"}`
+		SendHecEvent(t, endpoint, strings.NewReader(data))
+
+		c := e2e.GetClient(t)
+		ent := e2e.WaitForEntries(t, c, fmt.Sprintf(`tag=gravwell syslog Appname==httpingester Message=="HEC request" bytes==%d`, len(data)), time.Minute, 1, 30*time.Second)
+		if len(ent) != 1 {
+			e2e.Fatalf(t, "got %d entries, want 1", len(ent))
+		}
+	})
 }
 
-func SendHec(t *testing.T, endpoint string, data io.Reader) {
-	req, err := http.NewRequest("POST", endpoint+"/services/collector/event", data)
+func SendHecEvent(t *testing.T, endpoint string, data io.Reader) {
+	send(t, endpoint+"/services/collector/event", data)
+}
+
+func SendHecRaw(t *testing.T, endpoint string, data io.Reader) {
+	send(t, endpoint+"/services/collector/raw", data)
+}
+
+func send(t *testing.T, endpoint string, data io.Reader) {
+	req, err := http.NewRequest("POST", endpoint, data)
 	if err != nil {
 		t.Fatal(err)
 	}
