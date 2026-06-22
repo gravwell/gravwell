1362cc9a095a65ea3dff751d659fa9752e734ca5
John Floren <john@jfloren.net>
2026-06-22 13:15:53 -0700

    remove excessive caution

--- gwcli/tree/dashboards/dashboards.go
+++ gwcli/tree/dashboards/dashboards.go
@@ -70,15 +70,9 @@ func list(fs *pflag.FlagSet) ([]types.Dashboard, error) {
 		clilog.GetFlag(err)
 	} else if all {
 		r, err := connection.Client.ListAllDashboards(nil)
-		if err != nil {
-			return nil, err
-		}
 		return r.Results, err
 	}
 	r, err := connection.Client.ListDashboards(nil)
-	if err != nil {
-		return nil, err
-	}
 	return r.Results, err
 }
 
