fb135abbe0cee2f4aac7ab5c79a1ad65fffe7bd0
Tyler Elton <tyler.elton@gravwell.io>
2026-08-17 13:42:36 -0400

    chore: interface -> any

--- ingest/config/validate/validate.go
+++ ingest/config/validate/validate.go
@@ -33,17 +33,17 @@ var (
 // The first value is an opaque object (hence the reflect voodoo).  The second value is an error object
 // the point of this function is to make it easy for ingester writers to just hand in their GetConfig function
 // and the two paths and get a "go/no go" on the configurations.
-func ValidateConfig(fnc interface{}, pth, confdPath string) {
+func ValidateConfig(fnc any, pth, confdPath string) {
 	validateConfig(fnc, pth, confdPath, false) // this is used by NOT ingesters
 }
 
 // ValidateIngesterConfig behaves same as ValidateConfig but also asserts that the provided config
 // can return an IngestBaseConfig object.
-func ValidateIngesterConfig(fnc interface{}, pth, confdPath string) {
+func ValidateIngesterConfig(fnc any, pth, confdPath string) {
 	validateConfig(fnc, pth, confdPath, true) // this is used by ingesters
 }
 
-func validateConfig(fnc interface{}, pth, confdPath string, assertIngester bool) {
+func validateConfig(fnc any, pth, confdPath string, assertIngester bool) {
 	if !*vflag {
 		return
 	}
@@ -117,7 +117,7 @@ type igstConfig interface {
 	IngestBaseConfig() config.IngestConfig
 }
 
-func callVerifyFunc(obj interface{}) (err error) {
+func callVerifyFunc(obj any) (err error) {
 	var ok bool
 	var vv validator
 	if obj == nil {
