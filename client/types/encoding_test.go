04ef83a3e52a85c6ea24a567a9468b35b5b9caf0
rory-landau-gravwell <rory.landau@gravwell.io>
2026-08-20 19:25:17 -0700

    add test

--- client/types/encoding_test.go
+++ client/types/encoding_test.go
@@ -1,13 +1,18 @@
-package types
+package types_test
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/gravwell/gravwell/v4/client/types"
+	"github.com/stretchr/testify/require"
 )
 
 func TestJsonMaxDate(t *testing.T) {
-	si := ShardInfo{
+	si := types.ShardInfo{
 		Start: time.Date(10001, time.January, 1, 0, 0, 0, 0, time.UTC),
 		End:   time.Date(10001, time.January, 2, 0, 0, 0, 0, time.UTC),
 	}
@@ -15,14 +20,14 @@ func TestJsonMaxDate(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	var x ShardInfo
+	var x types.ShardInfo
 	if err := json.Unmarshal(b, &x); err != nil {
 		t.Fatal(err)
-	} else if x.Start != maxJsonTimestamp || x.End != maxJsonTimestamp {
+	} else if x.Start != types.MaxJSONTimestamp || x.End != types.MaxJSONTimestamp {
 		t.Fatalf("Invalid timestamps on future shard: %v, %v", x.Start, x.End)
 	}
 
-	si = ShardInfo{
+	si = types.ShardInfo{
 		Start: time.Now(),
 		End:   time.Now().Add(1 * time.Hour),
 	}
@@ -34,6 +39,34 @@ func TestJsonMaxDate(t *testing.T) {
 		t.Fatal(err)
 	} else if x.Start.Unix() != si.Start.Unix() || x.End.Unix() != si.End.Unix() {
 		t.Fatalf("Invalid timestamps on current shard: %v != %v, %v != %v", x.Start, si.Start, x.End, si.End)
+	}
+
+}
+
+func TestMarshalingNilCreatesEmpty(t *testing.T) {
+	var sb strings.Builder
+	enc := json.NewEncoder(&sb)
+	enc.SetIndent("", "\t")
+
+	var tests = []struct {
+		data any
+	}{
+		{types.Dashboard{}},
+		//{types.BaseListResponse{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%T", tt.data), func(t *testing.T) {
+			sb.Reset()
+			err := enc.Encode(&tt.data)
+			require.Nil(t, err)
+
+			out := sb.String()
+			if strings.Contains(out, "null") {
+				t.Fatal("found null in JSON:\n", out)
+			}
+		})
+
 	}
 
 }
