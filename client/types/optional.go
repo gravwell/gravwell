af25099bae49cf84bed1dac3f24d6fa216ed6b97
rory-landau-gravwell <rory.landau@gravwell.io>
2026-08-25 20:09:42 -0700

    introduce Optional type

---
+++ client/types/optional.go
@@ -0,0 +1,49 @@
+package types
+
+import "encoding/json"
+
+// An Optional type represents a field which may be unset during an update, preserving its prior value.
+// If !Optional.IsSet(), this field will be omitted from a JSON marshal of the type.
+type Optional[T any] struct {
+	value T
+	set   bool
+}
+
+// NewOptional returns a set field with value v.
+func NewOptional[T any](v T) Optional[T] {
+	return Optional[T]{value: v, set: true}
+}
+
+// Set installs the given value and marks it as valid to include when marshaling.
+func (o Optional[T]) Set(v T) {
+	o.value = v
+	o.set = true
+}
+
+// Value fetches the value contained within the Optional.
+// It is intended to prevent nil-derefs and is always safe to call.
+// If !o.IsSet(), T's zero value will be returned
+func (o Optional[T]) Value() T {
+	if !o.IsSet() {
+		var zero T
+		return zero
+	}
+	return o.value
+}
+
+// IsSet states if this field has a value and thus will be included in the JSON representation.
+func (o Optional[T]) IsSet() bool {
+	return o.set
+}
+
+// Unset marks this field s.t. it will be skipped when marshaling.
+func (o Optional[T]) Unset() {
+	o.set = false
+}
+
+func (o Optional[T]) MarshalJSON() ([]byte, error) {
+	if !o.IsSet() {
+		return nil, nil
+	}
+	return json.Marshal(o.value)
+}
