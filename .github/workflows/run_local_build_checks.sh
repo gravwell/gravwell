e5acc7536b7688ade5801d605f0d74f39b9abc22
Ashley Wade <107071905+ashnwade@users.noreply.github.com>
2026-08-27 15:10:07 -0400

    updated pinned version

--- .github/workflows/run_local_build_checks.sh
+++ .github/workflows/run_local_build_checks.sh
@@ -8,7 +8,7 @@ go mod tidy
 go mod download
 go mod verify
 go install golang.org/x/vuln/cmd/govulncheck@latest
-go install honnef.co/go/tools/cmd/staticcheck@ff63afafc529279f454e02f1d060210bd4263951
+go install honnef.co/go/tools/cmd/staticcheck@2026.2.1
 
 echo "Running go vet"
 	go vet ./generators/ipgen
