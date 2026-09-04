895fbf2b474134940da6b48ff3e197edaf476949
rory-landau-gravwell <rory.landau@gravwell.io>
2026-09-04 08:33:59 -0700

    correct copyright year; use external test package

--- client/optional_test.go
+++ client/optional_test.go
@@ -1,12 +1,12 @@
 /*************************************************************************
- * Copyright 2021 Gravwell, Inc. All rights reserved.
+ * Copyright 2026 Gravwell, Inc. All rights reserved.
  * Contact: <legal@gravwell.io>
  *
  * This software may be modified and distributed under the terms of the
  * BSD 2-clause license. See the LICENSE file for details.
  **************************************************************************/
 
-package client
+package client_test
 
 import (
 	"testing"
