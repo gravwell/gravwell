87d0ac00152dbe0f54da7be601e8e5e0eb89b520
rory-landau-gravwell <rory.landau@gravwell.io>
2026-09-16 17:29:00 -0700

    correct parameter

--- gwcli/connection/connection_noci_test.go
+++ gwcli/connection/connection_noci_test.go
@@ -463,7 +463,7 @@ func TestJWTRefreshing(t *testing.T) {
 		t.Error("token file was not updated while we were sleeping")
 	}
 	// validate that we can still make calls
-	_, err = connection.Client.ListKits(nil)
+	_, err = connection.Client.ListKits(types.QueryOptions{})
 	if err != nil {
 		t.Error("client failed to fetch kits:", err)
 	}
